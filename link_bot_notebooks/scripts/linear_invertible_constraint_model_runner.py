4176f430af00be7b1e7d8cd7bb99724c5c798e48
Peter Mitrano <pmitrano@umich.edu>
2019-01-17 13:49:57 -0500

    rewrite data loading

--- link_bot_notebooks/scripts/linear_invertible_constraint_model_runner.py
+++ link_bot_notebooks/scripts/linear_invertible_constraint_model_runner.py
@@ -2,20 +2,21 @@
 from __future__ import print_function
 
 import argparse
+import re
 import os
 import numpy as np
 
 from link_bot_notebooks import toy_problem_optimization_common as tpo
-from link_bot_notebooks import linear_tf_model
+from link_bot_notebooks import linear_invertible_constraint_model
 from link_bot_notebooks import experiments_util
 
 DT = 0.1
 
 
 def train(args):
-    model = linear_tf_model.LinearTFModel(vars(args), args.N, args.M, args.L, n_steps=args.n_steps, dt=DT)
+    model = linear_invertible_constraint_model.LinearInvertibleModel(vars(args), N=args.N, M=args.M, K=args.K,
+                                                                     L=args.L, dt=DT)
 
-    # goal = np.array([[0], [0], [0], [1], [0], [2]])
     goals = []
     for _ in range(args.n_goals):
         x = np.random.uniform(-5, 5)
@@ -32,8 +33,28 @@ def train(args):
     model.setup()
 
     log_path = experiments_util.experiment_name(args.log)
-    x = tpo.load_train(args.dataset, n_steps=args.n_steps, N=args.N, L=args.L,
-                       extract_func=tpo.link_pos_vel_extractor2(args.N))
+    log_data = np.loadtxt(args.dataset)
+    matches = re.search(r"(\d+)_(\d+)_.*", args.dataset)
+    if not matches:
+        print("could not parse dataset name")
+        return
+    try:
+        number_of_trajectories_during_collection = int(matches.group(1))
+        trajectory_length_during_collection = int(matches.group(2))
+        n_points = number_of_trajectories_during_collection * (trajectory_length_during_collection + 1)
+    except ValueError:
+        print("could not convert regex matches in filename to integers")
+        return
+
+    if n_points != log_data.shape[0]:
+        print("ERROR: number of data points based on file name {}*{}={} does not match shape of data {}".format(
+            number_of_trajectories_during_collection,
+            trajectory_length_during_collection,
+            n_points,
+            log_data.shape[0]))
+        return
+
+    x = tpo.load_train2(log_data, tpo.link_pos_vel_extractor2_indeces(), trajectory_length_during_collection, 1)
 
     for goal in goals:
         interrupted = model.train(x, goal, args.epochs, log_path)
@@ -46,7 +67,8 @@ def train(args):
 
 
 def model_only(args):
-    model = linear_tf_model.LinearTFModel(vars(args), N=args.N, M=args.M, L=args.L, n_steps=args.n_steps, dt=DT)
+    model = linear_invertible_constraint_model.LinearInvertibleModel(vars(args), N=args.N, M=args.M, K=args.K,
+                                                                     L=args.L, dt=DT)
     if args.log:
         model.init()
         log_path = experiments_util.experiment_name(args.log)
@@ -56,10 +78,9 @@ def model_only(args):
 
 def evaluate(args):
     goal = np.array([[0], [0], [0], [1], [0], [2]])
-    x = tpo.load_train(args.dataset, n_steps=args.n_steps, N=args.N, L=args.L,
-                       extract_func=tpo.link_pos_vel_extractor2(args.N))
-    print(x.shape)
-    model = linear_tf_model.LinearTFModel(vars(args), N=args.N, M=args.M, L=args.L, n_steps=args.n_steps, dt=0.1)
+    x = tpo.load_train(args.dataset, N=args.N, L=args.L, extract_func=tpo.link_pos_vel_extractor2(args.N))
+    model = linear_invertible_constraint_model.LinearInvertibleModel(vars(args), N=args.N, M=args.M, K=args.K,
+                                                                     L=args.L, dt=0.1)
     model.load()
     model.evaluate(x, goal)
 
@@ -70,7 +91,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbose", action='store_true')
     parser.add_argument("-N", help="dimensions in input state", type=int, default=6)
-    parser.add_argument("-M", help="dimensions in latent state", type=int, default=2)
+    parser.add_argument("-M", help="dimensions in latent state", type=int, default=4)
+    parser.add_argument("-K", help="dimensions in latent state", type=int, default=2)
     parser.add_argument("-L", help="dimensions in control input", type=int, default=2)
 
     subparsers = parser.add_subparsers()
@@ -82,7 +104,6 @@ if __name__ == '__main__':
     train_subparser.add_argument("--batch-size", "-b", type=int, default=1024)
     train_subparser.add_argument("--print-period", "-p", type=int, default=100)
     train_subparser.add_argument("--n-goals", "-n", type=int, default=500)
-    train_subparser.add_argument("--n-steps", "-s", type=int, default=1)
     train_subparser.add_argument("--tf-debug", action="store_true")
     train_subparser.set_defaults(func=train)
 
