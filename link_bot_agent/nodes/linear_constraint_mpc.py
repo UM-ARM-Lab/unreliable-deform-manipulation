fixing collision masking
previously it was just masking out anything where k_label = 1
but we actually want to mask out data from ALL time steps after (and
including) the first time k_label=1

link_bot_agent/nodes/linear_constraint_mpc.py
@@ -260,7 +260,7 @@ def eval(args):
     stats_filename = os.path.join(os.path.dirname(args.checkpoint), 'eval_{}.txt'.format(int(timemod.time())))
     start = np.array([[-1, 0, 0, 0, 0, 0]])
 
-    min_costs, execution_times, nums_contacts, num_fails, num_successes = common(args, start, 100)
+    min_costs, execution_times, nums_contacts, num_fails, num_successes = common(args, start, 200)
 
     eval_stats_lines = [
         '% fail: {}'.format(float(num_fails) / args.n_trials),

link_bot_notebooks/scripts/linear_constraint_model_runner.py
@@ -39,11 +39,15 @@ def model_only(args):
     fake_sdf_res = np.random.randn(2).astype(np.float32)
     model = m.LinearConstraintModel(vars(args), fake_sdf, fake_sdf_grad, fake_sdf_res, 250, args.N, args.M, args.L,
                                     args.P, args.Q, 0.1, 50)
+
+    model.init()
+
     if args.log:
-        model.init()
         log_path = experiments_util.experiment_name(args.log)
         full_log_path = os.path.join(os.getcwd(), "log_data", log_path)
         model.save(full_log_path)
+
+    print(model)
 
 
 def evaluate(args):
@@ -83,6 +87,7 @@ def main():
     parser.add_argument("-P", help="dimensions in latent state o_k", type=int, default=2)
     parser.add_argument("-Q", help="dimensions in constraint checking output space", type=int, default=1)
     parser.add_argument("--debug", help="enable TF Debugger", action='store_true')
+    parser.add_argument("--seed", type=int, default=0)
 
     subparsers = parser.add_subparsers()
     train_subparser = subparsers.add_parser("train")
@@ -93,7 +98,6 @@ def main():
     train_subparser.add_argument("--checkpoint", "-c", help="restart from this *.ckpt name")
     train_subparser.add_argument("--print-period", "-p", type=int, default=200)
     train_subparser.add_argument("--save-period", type=int, default=400)
-    train_subparser.add_argument("--seed", type=int, default=0)
     train_subparser.set_defaults(func=train)
 
     eval_subparser = subparsers.add_parser("eval")

link_bot_notebooks/src/link_bot_notebooks/linear_constraint_model.py
@@ -13,6 +13,22 @@ from link_bot_notebooks import toy_problem_optimization_common as tpo
 from tensorflow.python import debug as tf_debug
 
 
+def make_constraint_mask(arr, axis=1):
+    """ takes in a 2d array and returns two lists of indeces all elements before the first constriant violation """
+    arr = arr.squeeze()
+    invalid_val = arr.shape[1]
+    mask = arr != 0
+    indeces_of_first_violation = np.where(mask.any(axis=axis), mask.argmax(axis=axis), invalid_val)
+    batch_indeces = []
+    time_indeces = []
+    for batch_index, index_of_first_violation in enumerate(indeces_of_first_violation):
+        for index_with_no_violation in range(index_of_first_violation):
+            batch_indeces.append(batch_index)
+            time_indeces.append(index_with_no_violation)
+
+    return batch_indeces, time_indeces
+
+
 @tf.custom_gradient
 def sdf_func(sdf, full_sdf_gradient, resolution, sdf_origin_coordinate, sdf_coordinates, P, Q):
     integer_coordinates = tf.cast(tf.divide(sdf_coordinates, resolution), dtype=tf.int32)
@@ -63,6 +79,7 @@ class LinearConstraintModel(base_model.BaseModel):
         self.s_goal = tf.placeholder(tf.float32, shape=(1, N), name="s_goal")
         self.c_label = tf.placeholder(tf.float32, shape=(batch_size, self.n_steps + 1), name="c")
         self.k_label = tf.placeholder(tf.float32, shape=(batch_size, self.n_steps + 1, Q), name="k")
+        self.k_mask_indeces_2d = tf.placeholder(tf.int32, shape=(None, 2), name="k_mask_indeces_2d")
         self.k_label_int = tf.cast(self.k_label, tf.int32)
 
         # R_d_init = np.random.randn(N, M).astype(np.float32) * 1e-1
@@ -79,7 +96,7 @@ class LinearConstraintModel(base_model.BaseModel):
         R_d_init[1, 1] = 1
         A_d_init = np.zeros((M, M), dtype=np.float32)
         B_d_init = np.zeros((M, L), dtype=np.float32)
-        np.fill_diagonal(B_d_init, 1)
+        np.fill_diagonal(B_d_init, 0.3)
         R_k_init = np.zeros((N, P), dtype=np.float32)
         R_k_init[N - 2, 0] = 1.0
         R_k_init[N - 1, 1] = 1.0
@@ -134,28 +151,37 @@ class LinearConstraintModel(base_model.BaseModel):
         # NOTE: we use a mask to set the state prediction loss to 0 when the constraint is violated?
         # this way we don't penalize our model for failing to predict the dynamics in collision
         # the (1-label) inverts the labels so that 0 means in collision and mask out
-        self.constraint_label_mask = 1 - tf.squeeze(self.k_label)
         # TODO: this is invalid for when Q is not 1
-        self.mask_indeces_2d = tf.where(1 - tf.reshape(self.k_label, [self.batch_size, self.n_steps + 1]))
-        self.mask_indeces_3d = tf.where(1 - self.k_label)
 
         with tf.name_scope("train"):
             # sum of squared errors in latent space at each time step
             with tf.name_scope("latent_dynamics_d"):
-                self.all_state_prediction_error_in_d = tf.reduce_sum(tf.pow(self.hat_o_d - self.hat_o_d_next, 2), axis=2)
-                self.state_prediction_error_in_d = tf.gather_nd(self.all_state_prediction_error_in_d, self.mask_indeces_2d, name='all_state_prediction_error_in_d')
-                self.state_prediction_loss_in_d = tf.reduce_mean(self.state_prediction_error_in_d, name='state_prediction_loss_in_d')
+                self.all_state_prediction_error_in_d = tf.reduce_sum(tf.pow(self.hat_o_d - self.hat_o_d_next, 2),
+                                                                     axis=2)
+                self.state_prediction_error_in_d = tf.gather_nd(self.all_state_prediction_error_in_d,
+                                                                self.k_mask_indeces_2d,
+                                                                name='all_state_prediction_error_in_d')
+                self.state_prediction_loss_in_d = tf.reduce_mean(self.state_prediction_error_in_d,
+                                                                 name='state_prediction_loss_in_d')
                 self.all_cost_prediction_error = tf.square(self.hat_c - self.c_label, name='all_cost_prediction_error')
-                self.cost_prediction_error = tf.gather_nd(self.all_cost_prediction_error, self.mask_indeces_2d, name='cost_prediction_error')
+                self.cost_prediction_error = tf.gather_nd(self.all_cost_prediction_error, self.k_mask_indeces_2d,
+                                                          name='cost_prediction_error')
                 self.cost_prediction_loss = tf.reduce_mean(self.cost_prediction_error, name='cost_prediction_loss')
 
             with tf.name_scope("latent_constraints_k"):
-                self.all_state_prediction_error_in_k = tf.reduce_sum(tf.pow(self.hat_o_k - self.hat_o_k_next, 2), axis=2)
-                self.state_prediction_error_in_k = tf.gather_nd(self.all_state_prediction_error_in_k, self.mask_indeces_2d, name='all_state_prediction_error_in_k')
-                self.state_prediction_loss_in_k = tf.reduce_mean(self.state_prediction_error_in_k, name='state_prediction_loss_in_k')
+                self.all_state_prediction_error_in_k = tf.reduce_sum(tf.pow(self.hat_o_k - self.hat_o_k_next, 2),
+                                                                     axis=2)
+                self.state_prediction_error_in_k = tf.gather_nd(self.all_state_prediction_error_in_k,
+                                                                self.k_mask_indeces_2d,
+                                                                name='all_state_prediction_error_in_k')
+                self.state_prediction_loss_in_k = tf.reduce_mean(self.state_prediction_error_in_k,
+                                                                 name='state_prediction_loss_in_k')
 
-                self.constraint_prediction_error = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.hat_k, labels=self.k_label, name='constraint_prediction_error')
-                self.constraint_prediction_loss = tf.reduce_mean(self.constraint_prediction_error, name="constraint_prediction_loss")
+                self.constraint_prediction_error = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.hat_k,
+                                                                                           labels=self.k_label,
+                                                                                           name='constraint_prediction_error')
+                self.constraint_prediction_loss = tf.reduce_mean(self.constraint_prediction_error,
+                                                                 name="constraint_prediction_loss")
 
             self.flat_weights = tf.concat(
                 (tf.reshape(self.R_d, [-1]), tf.reshape(self.A_d, [-1]), tf.reshape(self.B_d, [-1])), axis=0)
@@ -241,7 +267,8 @@ class LinearConstraintModel(base_model.BaseModel):
                          self.u: u,
                          self.s_goal: goal,
                          self.c_label: c,
-                         self.k_label: k}
+                         self.k_label: k,
+                         self.k_mask_indeces_2d: make_constraint_mask(k)}
 
             ops = [self.global_step, self.summaries, self.loss, self.opt]
             for i in range(epochs):
@@ -283,7 +310,8 @@ class LinearConstraintModel(base_model.BaseModel):
                      self.u: u,
                      self.s_goal: goal,
                      self.c_label: c,
-                     self.k_label: k}
+                     self.k_label: k,
+                     self.k_mask_indeces_2d: make_constraint_mask(k)}
         ops = [self.R_d, self.A_d, self.B_d, self.D, self.R_k, self.A_k, self.B_k, self.threshold_k,
                self.state_prediction_loss_in_d, self.state_prediction_loss_in_k, self.cost_prediction_loss,
                self.constraint_prediction_loss, self.regularization, self.loss, self.constraint_prediction_accuracy]

link_bot_notebooks/test/test_linear_constraint_model.py
@@ -13,7 +13,7 @@ class TestLoss(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         dt = 0.1
-        batch_size = 1
+        batch_size = 2
         n_steps = 2
 
         args = {
@@ -34,7 +34,7 @@ class TestLoss(unittest.TestCase):
         W = 10
         H = 20
         sdf = np.ones((H, W), dtype=np.float32)
-        sdf[H // 2 + 2, W // 2 + -3] = -1
+        sdf[H // 2 + 1, W // 2 + 3] = -1
         sdf_gradient = np.zeros((W, H, 2), dtype=np.float32)
         sdf_resolution = np.array([1, 1], dtype=np.float32)
 
@@ -70,92 +70,120 @@ class TestLoss(unittest.TestCase):
 
         # Construct inputs
         s = np.array([
-            [3, 7, 4],
-            [1, 2, 3],
-            [2, -1, -3],
+            [[3, 7, 4],
+             [1, 2, 3],
+             [2, -1, -3]],
+            [[-1, 2, -1],
+             [1, 1, 2],
+             [1, 2, -2]],
         ])
 
         u = np.array([
-            [0, 1],
-            [1, 1],
+            [[0, 1],
+             [1, 1]],
+            [[1, 0],
+             [1, 0]],
         ])
 
         cls.c = np.linalg.norm(s - cls.goal, axis=1)
         cls.k = np.array([
-            [0],
-            [0],
-            [1],
+            [[0],
+             [1],
+             [0]],
+            [[0],
+             [0],
+             [0]],
         ])
 
         cls.feed_dict = {
-            cls.model.s: np.expand_dims(s, axis=0),
-            cls.model.u: np.expand_dims(u, axis=0),
+            cls.model.s: s,
+            cls.model.u: u,
             cls.model.s_goal: cls.goal,
-            cls.model.c_label: np.expand_dims(cls.c, axis=0),
-            cls.model.k_label: np.expand_dims(cls.k, axis=0)
+            cls.model.c_label: cls.c,
+            cls.model.k_label: cls.k,
         }
 
     def test_hat_o_d(self):
-        expected_hat_o_d = np.array([[
-            [3, 7],
-            [1, 2],
-            [2, -1],
-        ]])
+        expected_hat_o_d = np.array([
+            [[3, 7],
+             [1, 2],
+             [2, -1]],
+            [[-1, 2],
+             [1, 1],
+             [1, 2]],
+        ])
 
         hat_o_d = self.model.sess.run(self.model.hat_o_d, feed_dict=self.feed_dict)
         np.testing.assert_allclose(hat_o_d, expected_hat_o_d)
 
     def test_hat_o_d_next(self):
-        expected_hat_o_d_next = np.array([[
-            [3, 7],
-            [3, 7.1],
-            [3.1, 7.2],
-        ]])
+        expected_hat_o_d_next = np.array([
+            [[3, 7],
+             [3, 7.1],
+             [3.1, 7.2]],
+            [[-1, 2],
+             [-0.9, 2],
+             [-0.8, 2]],
+        ])
 
         hat_o_d_next = self.model.sess.run(self.model.hat_o_d_next, feed_dict=self.feed_dict)
         np.testing.assert_allclose(hat_o_d_next, expected_hat_o_d_next)
 
     def test_hat_o_k(self):
-        expected_hat_o_k = np.array([[
-            [3, 4],
-            [1, 3],
-            [2, -3],
-        ]])
+        expected_hat_o_k = np.array([
+            [[3, 4],
+             [1, 3],
+             [2, -3]],
+            [[-1, -1],
+             [1, 2],
+             [1, -2]],
+        ])
 
         hat_o_k = self.model.sess.run(self.model.hat_o_k, feed_dict=self.feed_dict)
         np.testing.assert_allclose(hat_o_k, expected_hat_o_k)
 
     def test_hat_o_k_next(self):
-        expected_hat_o_k_next = np.array([[
-            [3, 4],
-            [3, 4.1],
-            [3.1, 4.2],
-        ]])
+        expected_hat_o_k_next = np.array([
+            [[3, 4],
+             [3, 4.1],
+             [3.1, 4.2]],
+            [[-1, -1],
+             [-0.9, -1],
+             [-0.8, -1]],
+        ])
 
         hat_o_k_next = self.model.sess.run(self.model.hat_o_k_next, feed_dict=self.feed_dict)
         np.testing.assert_allclose(hat_o_k_next, expected_hat_o_k_next)
 
     def test_error_to_goal(self):
-        expected_d_to_goal = np.array([[
-            [2 - 3, 1 - 7],
-            [2 - 1, 1 - 2],
-            [2 - 2, 1 - -1],
-        ]])
+        expected_d_to_goal = np.array([
+            [[2 - 3, 1 - 7],
+             [2 - 1, 1 - 2],
+             [2 - 2, 1 - -1]],
+            [[2 - -1, 1 - 2],
+             [2 - 1, 1 - 1],
+             [2 - 1, 1 - 2]],
+        ])
 
         hat_d_to_goal = self.model.sess.run(self.model.d_to_goal, feed_dict=self.feed_dict)
         np.testing.assert_allclose(hat_d_to_goal, expected_d_to_goal)
 
     def test_cost_loss(self):
-        expected_c = np.linalg.norm(np.array([[
-            [2 - 3, 1 - 7],
-            [2 - 1, 1 - 2],
-            [2 - 2, 1 - -1],
-        ]]), axis=2) ** 2
+        expected_c = np.linalg.norm(np.array([
+            [[2 - 3, 1 - 7],
+             [2 - 1, 1 - 2],
+             [2 - 2, 1 - -1]],
+            [[2 - -1, 1 - 2],
+             [2 - 1, 1 - 1],
+             [2 - 1, 1 - 2]],
+        ]), axis=2) ** 2
 
         expected_c_error = (expected_c - self.c) ** 2
         expected_c_error_masked = np.array([
-            expected_c_error[0, 0],
-            expected_c_error[0, 1]
+            expected_c_error[0, 0],  # only the first time step should be un-masked
+            expected_c_error[1, 0],  # for the second trajectory, all steps are un-masked
+            expected_c_error[1, 1],
+            expected_c_error[1, 2],
         ])
         expected_c_loss = np.mean(expected_c_error_masked)
 
@@ -180,7 +208,6 @@ class TestLoss(unittest.TestCase):
 
         expected_state_prediction_error_in_d_masked = np.array([
             0,
-            (3 - 1) ** 2 + (7.1 - 2) ** 2,
         ])
 
         expected_sd_loss = np.mean(expected_state_prediction_error_in_d_masked)
@@ -206,7 +233,6 @@ class TestLoss(unittest.TestCase):
 
         expected_state_prediction_error_in_k_masked = np.array([
             0,
-            (3 - 1) ** 2 + (4.1 - 3) ** 2,
         ])
 
         expected_sd_loss = np.mean(expected_state_prediction_error_in_k_masked)
@@ -226,14 +252,14 @@ class TestLoss(unittest.TestCase):
     def test_constraint_loss(self):
         expected_k_violated = np.array([[
             [False],
-            [False],
             [True],
+            [False],
         ]])
 
         expected_k = np.array([[
             [-0.5],
-            [-0.5],
             [1.5],
+            [-0.5],
         ]]) * 100
 
         expected_k_loss = np.array([0], dtype=np.float32)