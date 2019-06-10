fix sdf gradient for random environments

link_bot_models/scripts/plot_constraint_map.py
@@ -18,7 +18,7 @@ def get_rope_configurations(args):
     if args.dataset:
         data = np.load(args.dataset)
         states = data['states']
-        N = states.shape[2]
+        N = states.shape[-1]
         assert N == 6
         rope_configurations = states.reshape(-1, N)
     else:
@@ -36,12 +36,14 @@ def get_rope_configurations(args):
 def plot(args, sdf_data, model, threshold, results, true_positives, true_negatives, false_positives, false_negatives):
     n_examples = results.shape[0]
 
+    sdf_data.image = (sdf_data.image < threshold).astype(np.uint8)
+
     if args.plot_type == plotting.PlotType.random_individual:
         random_indexes = np.random.choice(n_examples, size=10, replace=False)
         random_results = results[random_indexes]
         figs = []
         for random_result in random_results:
-            fig = plotting.plot_single_example(sdf_data.image, random_result)
+            fig = plotting.plot_single_example(sdf_data, random_result)
             figs.append(fig)
         return plotting.SavableFigureCollection(figs)
 
@@ -89,6 +91,7 @@ def main():
     parser.add_argument("sdf", help="sdf and gradient of the environment (npz file)")
     parser.add_argument("checkpoint", help="eval the *.ckpt name")
     parser.add_argument("plot_type", type=plotting.PlotType.from_string, choices=list(plotting.PlotType))
+    parser.add_argument("threshold", type=float)
     parser.add_argument("--verbose", action='store_true')
     parser.add_argument("--save", action='store_true')
     parser.add_argument("-N", help="dimensions in input state", type=int, default=6)
@@ -104,14 +107,12 @@ def main():
     model = ConstraintModel(args_dict, sdf_data, args.N)
     model.setup()
 
-    threshold = 0.2
-
     # get the rope configurations we're going to evaluate
     rope_configurations = get_rope_configurations(args)
     m = rope_configurations.shape[0]
 
     # evaluate the rope configurations
-    results = constraint_model.evaluate(sdf_data, model, threshold, rope_configurations)
+    results = constraint_model.evaluate(sdf_data, model, args.threshold, rope_configurations)
 
     true_positives = np.array([result for result in results if result.true_violated and result.predicted_violated])
     n_true_positives = len(true_positives)
@@ -129,7 +130,7 @@ def main():
     print('precision:', precision)
     print('recall:', recall)
 
-    savable = plot(args, sdf_data, model, threshold, results, true_positives, true_negatives,
+    savable = plot(args, sdf_data, model, args.threshold, results, true_positives, true_negatives,
                    false_positives, false_negatives)
 
     plt.show()

link_bot_models/scripts/random_environment_data.py
@@ -7,7 +7,7 @@ from link_bot_pycommon import link_bot_pycommon
 import sdf_tools
 
 
-def plot(args, grid_world, sdf_data, threshold, rope_configurations, constraint_labels):
+def plot(args, sdf_data, threshold, rope_configurations, constraint_labels):
     sdf_fig = plt.figure()
     # Note: images should always be flipped and transposed because of how image coordinates work.
     #       however, we do not flip/transpose when indexing into the SDF, this is just needed when plotting.
@@ -18,15 +18,18 @@ def plot(args, grid_world, sdf_data, threshold, rope_configurations, constraint_
     subsample = 16
     x_range = np.arange(sdf_data.extent[0], sdf_data.extent[1], subsample * sdf_data.resolution[0])
     y_range = np.arange(sdf_data.extent[0], sdf_data.extent[1], subsample * sdf_data.resolution[1])
-    x, y = np.meshgrid(x_range, y_range)
+    y, x = np.meshgrid(y_range, x_range)
     dx = sdf_data.gradient[::subsample, ::subsample, 0]
     dy = sdf_data.gradient[::subsample, ::subsample, 1]
     plt.quiver(x, y, dx, dy, units='x', scale=5, headwidth=2, headlength=4)
 
     grid_fig = plt.figure()
-    plt.imshow(np.flipud(grid_world.T) > threshold, extent=sdf_data.extent)
+    binary = (sdf_data.sdf < threshold).astype(np.uint8)
+    plt.imshow(np.flipud(binary.T), extent=sdf_data.extent)
 
-    for rope_configuration, constraint_label in zip(rope_configurations[:1000], constraint_labels[:1000]):
+    for idx in np.random.choice(rope_configurations.shape[0], size=1000):
+        rope_configuration = rope_configurations[idx]
+        constraint_label = constraint_labels[idx]
         xs = [rope_configuration[0], rope_configuration[2], rope_configuration[4]]
         ys = [rope_configuration[1], rope_configuration[3], rope_configuration[5]]
         plt.plot(xs, ys, linewidth=1, zorder=2)
@@ -80,7 +83,8 @@ def generate(args):
                  grid_world=grid_world,
                  states=rope_configurations,
                  rope_configurations=rope_configurations,
-                 constraints=constraint_labels)
+                 constraints=constraint_labels,
+                 threshold=args.distance_constraint_threshold)
         np.savez(args.outfile + '_sdf.npz',
                  sdf=sdf,
                  sdf_gradient=sdf_gradient,
@@ -89,20 +93,20 @@ def generate(args):
 
     if args.plot:
         sdf_data = link_bot_pycommon.SDF(sdf, sdf_gradient, sdf_resolution, sdf_origin, sdf_extent, None)
-        plot(args, grid_world, sdf_data, args.distance_constraint_threshold, rope_configurations, constraint_labels)
+        plot(args, sdf_data, args.distance_constraint_threshold, rope_configurations, constraint_labels)
 
         plt.show()
 
 
 def plot_main(args):
     data = np.load(args.data)
-    grid_world = data['grid_world']
     rope_configurations = data['rope_configurations']
+    threshold = data['threshold']
     constraint_labels = data['constraints']
 
     sdf_data = link_bot_pycommon.load_sdf_data(args.sdf)
 
-    plot(args, grid_world, sdf_data, args.distance_constraint_threshold, rope_configurations, constraint_labels)
+    plot(args, sdf_data, threshold, rope_configurations, constraint_labels)
 
     plt.show()
 
@@ -128,7 +132,6 @@ def main():
     plot_parser.set_defaults(func=plot_main)
     plot_parser.add_argument('data', help='generated file, npz')
     plot_parser.add_argument('sdf', help='generated file, npz')
-    plot_parser.add_argument('--distance-constraint-threshold', type=np.float32, default=0.0, help='constraint threshold')
 
     args = parser.parse_args()
 

link_bot_models/src/link_bot_models/constraint_model.py
@@ -31,6 +31,7 @@ class ConstraintModel(BaseModel):
         self.k_label = tf.placeholder(tf.float32, shape=(None, 1), name="k")
         self.k_label_int = tf.cast(self.k_label, tf.int32)
         self.hidden_layer_dims = None
+        self.fig = None
 
         self.sdf_data = sdf_data
 
@@ -83,8 +84,7 @@ class ConstraintModel(BaseModel):
             self.hidden_layer_dims = [128, 128]
             h = self.observations
             for layer_idx, hidden_layer_dim in enumerate(self.hidden_layer_dims):
-                h = tf.layers.dense(h, hidden_layer_dim, activation=tf.nn.relu,
-                                    name='hidden_layer_{}'.format(layer_idx))
+                h = tf.layers.dense(h, hidden_layer_dim, activation=tf.nn.relu, name='hidden_layer_{}'.format(layer_idx))
             self.hat_latent_k = tf.layers.dense(h, 2, activation=None, name='output_layer')
 
         #######################################################
@@ -115,11 +115,11 @@ class ConstraintModel(BaseModel):
             oob_down = self.hat_latent_k[:, 1] >= sdf_data.extent[3]
             self.out_of_bounds = tf.math.reduce_any(tf.stack((oob_up, oob_down, oob_left, oob_right), axis=1), axis=1, name='oob')
             self.in_bounds_value = tf.ones_like(self.distances_to_origin) * 0.0
-            self.distances_out_of_bounds = tf.where(self.out_of_bounds, self.distances_to_origin, self.in_bounds_value, name='distance_oob')
-            self.out_of_bounds_loss = tf.reduce_mean(self.distances_to_origin, name='out_of_bounds_loss')
+            self.distances_out_of_bounds = tf.where(self.out_of_bounds, self.distances_to_origin, self.in_bounds_value)
+            self.out_of_bounds_loss = tf.reduce_mean(self.distances_out_of_bounds, name='out_of_bounds_loss')
 
             self.loss = tf.add_n([
-                # self.constraint_prediction_loss,
+                self.constraint_prediction_loss,
                 self.out_of_bounds_loss,
             ], name='loss')
 
@@ -197,9 +197,6 @@ class ConstraintModel(BaseModel):
         ops = [self.threshold_k, self.constraint_prediction_loss, self.loss, self.constraint_prediction_accuracy]
         threshold_k, k_loss, loss, k_accuracy = self.sess.run(ops, feed_dict=feed_dict)
 
-        plotting.plot_examples_2(self.sdf_data, observations[::100], threshold_k, self)
-        plt.show()
-
         if display:
             print("Constraint Loss: {:0.3f}".format(float(k_loss)))
             print("Overall Loss: {:0.3f}".format(float(loss)))
@@ -207,6 +204,17 @@ class ConstraintModel(BaseModel):
             print("constraint prediction accuracy:\n{}".format(k_accuracy))
 
         return threshold_k, k_loss, loss
+
+    def start_train_hook(self):
+        self.fig = plt.figure()
+
+    def train_feed_hook(self, iteration, train_x_batch, train_y_batch):
+        pass
+        # if iteration % 1 == 0:
+        #     threshold_k = self.sess.run(self.threshold_k)
+        #     self.fig.clf()
+        #     plotting.plot_examples_on_fig(self.fig, self.sdf_data, train_x_batch[::10], train_y_batch[::10], threshold_k, self, draw_correspondences=True)
+        #     plt.pause(5)
 
     def violated(self, observations, sdf=None, sdf_resolution=None, sdf_origin=None):
         # unused parameters