fix precision/recall calcuation
cut down batch loading time by half

link_bot_models/scripts/constraint_sdf_runner.py
@@ -7,34 +7,27 @@ import sys
 
 import numpy as np
 import tensorflow as tf
-from colorama import Fore
 
-from link_bot_models import multi_environment_datasets
 from link_bot_models.constraint_sdf import ConstraintSDF
-from link_bot_models.multi_environment_datasets import MultiEnvironmentDataset
 from link_bot_models.label_types import LabelType
+from link_bot_models.multi_environment_datasets import MultiEnvironmentDataset
 from link_bot_pycommon import experiments_util
+
+label_types = [LabelType.SDF]
 
 
 def train(args):
     log_path = experiments_util.experiment_name(args.log)
+
     train_dataset = MultiEnvironmentDataset.load_dataset(args.train_dataset)
     validation_dataset = MultiEnvironmentDataset.load_dataset(args.validation_dataset)
     sdf_shape = train_dataset.sdf_shape
+
     model = ConstraintSDF(vars(args), sdf_shape, args.N)
-
     model.setup()
+    model.train(train_dataset, validation_dataset, label_types, args.epochs, log_path)
 
-    train_inputs, train_labels = multi_environment_datasets.make_inputs_and_labels(train_dataset.environments)
-
-    validation_inputs, validation_labels = multi_environment_datasets.make_inputs_and_labels(validation_dataset.environments)
-
-    model.train(train_inputs, train_labels, validation_inputs, validation_labels, args.epochs, log_path)
-
-    print(Fore.GREEN + "\nTrain Evaluation" + Fore.RESET)
-    model.evaluate(train_inputs, train_labels)
-    print(Fore.GREEN + "\nValidation Evaluation" + Fore.RESET)
-    model.evaluate(validation_inputs, validation_labels)
+    model.evaluate(validation_dataset, label_types)
 
 
 def model_only(args):
@@ -62,10 +55,7 @@ def evaluate(args):
     model = ConstraintSDF(args_dict, sdf_shape, args.N)
     model.setup()
 
-    # take all the data as validation data
-    validation_inputs, validation_labels = multi_environment_datasets.make_inputs_and_labels(dataset.environments)
-
-    return model.evaluate(validation_inputs, validation_labels)
+    return model.evaluate(dataset, label_types)
 
 
 def show(args):
@@ -77,7 +67,7 @@ def show(args):
 
 
 def main():
-    np.set_printoptions(precision=6, suppress=True)
+    np.set_printoptions(precision=6, suppress=True, linewidth=220)
     tf.logging.set_verbosity(tf.logging.ERROR)
 
     parser = argparse.ArgumentParser()
@@ -85,19 +75,19 @@ def main():
     parser.add_argument("-N", help="dimensions in input state", type=int, default=6)
     parser.add_argument("--debug", help="enable TF Debugger", action='store_true')
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("label_type", type=LabelType.from_string, choices=list(LabelType))
+    # parser.add_argument("label_type", type=LabelType.from_string, choices=list(LabelType))
 
     subparsers = parser.add_subparsers()
     train_subparser = subparsers.add_parser("train")
     train_subparser.add_argument("train_dataset", help="dataset (json file)")
     train_subparser.add_argument("validation_dataset", help="dataset (json file)")
-    train_subparser.add_argument("--batch-size", "-b", type=int, default=128)
+    train_subparser.add_argument("--batch-size", "-b", type=int, default=200)
     train_subparser.add_argument("--log", "-l", nargs='?', help="save/log the graph and summaries", const="")
     train_subparser.add_argument("--epochs", "-e", type=int, help="number of epochs to train for", default=250)
     train_subparser.add_argument("--checkpoint", "-c", help="restart from this *.ckpt name")
-    train_subparser.add_argument("--log-period", type=int, default=500)
-    train_subparser.add_argument("--print-period", type=int, default=500)
-    train_subparser.add_argument("--val-period", type=int, default=20, help='run validation every so many epochs')
+    train_subparser.add_argument("--log-period", type=int, default=250)
+    train_subparser.add_argument("--print-period", type=int, default=250)
+    train_subparser.add_argument("--val-period", type=int, default=10, help='run validation every so many epochs')
     train_subparser.add_argument("--save-period", type=int, default=20, help='save every so many epochs')
     train_subparser.add_argument("--random-init", action='store_true')
     train_subparser.set_defaults(func=train)
@@ -105,6 +95,7 @@ def main():
     eval_subparser = subparsers.add_parser("eval")
     eval_subparser.add_argument("dataset", help="dataset (json file)")
     eval_subparser.add_argument("checkpoint", help="eval the *.ckpt name")
+    eval_subparser.add_argument("--batch-size", "-b", type=int, default=100)
     eval_subparser.set_defaults(func=evaluate)
 
     show_subparser = subparsers.add_parser("show")

link_bot_models/scripts/plot_constraint_model.py
@@ -13,7 +13,6 @@ from link_bot_models import constraint_sdf
 from link_bot_models import plotting
 from link_bot_models.constraint_sdf import ConstraintSDF
 from link_bot_models.multi_environment_datasets import MultiEnvironmentDataset
-from link_bot_models.label_types import LabelType
 
 
 def plot(args, sdf_data, model, threshold, results, true_positives, true_negatives, false_positives, false_negatives):
@@ -76,7 +75,7 @@ def main():
     parser.add_argument("dataset", help='use this dataset instead of random rope configurations')
     parser.add_argument("checkpoint", help="eval the *.ckpt name")
     parser.add_argument("threshold", type=float)
-    parser.add_argument("label_type", type=LabelType.from_string, choices=list(LabelType))
+    # parser.add_argument("label_type", type=LabelType.from_string, choices=list(LabelType))
     parser.add_argument("plot_type", type=plotting.PlotType.from_string, choices=list(plotting.PlotType))
     parser.add_argument("--verbose", action='store_true')
     parser.add_argument("--save", action='store_true')
@@ -102,26 +101,23 @@ def main():
         results = constraint_sdf.test_predictions(model, environment)
         m = results.shape[0]
 
-        n_positives = len([result for result in results if np.any(result.true_violated)])
-        n_negatives = len([result for result in results if not np.any(result.true_violated)])
-        print(n_positives, n_negatives)
-
-        true_positives = [result for result in results if np.any(result.true_violated) and result.predicted_violated]
+        label_mask = np.array([1, 0])
+        true_positives = [result for result in results if label_mask @ result.true_violated and result.predicted_violated]
         true_positives = np.array(true_positives)
         n_true_positives = len(true_positives)
-        false_positives = [result for result in results if np.any(result.true_violated) and not result.predicted_violated]
+        false_positives = [result for result in results if not label_mask @ result.true_violated and result.predicted_violated]
         false_positives = np.array(false_positives)
         n_false_positives = len(false_positives)
-        true_negatives = [result for result in results if not np.any(result.true_violated) and not result.predicted_violated]
+        true_negatives = [result for result in results if not label_mask @ result.true_violated and not result.predicted_violated]
         true_negatives = np.array(true_negatives)
         n_true_negatives = len(true_negatives)
-        false_negatives = [result for result in results if not np.any(result.true_violated) and result.predicted_violated]
+        false_negatives = [result for result in results if label_mask @ result.true_violated and not result.predicted_violated]
         false_negatives = np.array(false_negatives)
         n_false_negatives = len(false_negatives)
 
         accuracy = (n_true_positives + n_true_negatives) / m * 100
         precision = n_true_positives / (n_true_positives + n_false_positives) * 100
-        recall = n_true_negatives / (n_true_negatives + n_false_negatives) * 100
+        recall = n_true_positives / (n_true_positives + n_false_negatives) * 100
         print("precision: {:4.1f}%".format(precision))
         print("recall: {:4.1f}%".format(recall))
         print(Style.BRIGHT + "accuracy: {:4.1f}%".format(accuracy) + Style.NORMAL)

link_bot_models/src/link_bot_models/base_model.py
@@ -1,5 +1,6 @@
 import json
 import os
+from time import time
 
 import numpy as np
 import tensorflow as tf
@@ -65,18 +66,7 @@ class BaseModel:
     def init(self):
         self.sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
 
-    def train(self, train_x, train_y, validation_x, validation_y, epochs, log_path, **kwargs):
-        """
-
-        :param train_x: first dimension is each type of input and the second dimension is examples, following dims are the data
-        :param train_y: first dimension is type of label, second dimension is examples, following dims are the labels
-        :param validation_x: ''
-        :param validation_y: ''
-        :param epochs: number of times to run through the full training set
-        :param log_path:
-        :param kwargs:
-        :return: whether the training process was interrupted early (by Ctrl+C)
-        """
+    def train(self, train_dataset, validation_dataset, label_types, epochs, log_path, **kwargs):
         if not self.finish_setup_called:
             raise FinishSetupNotCalledInConstructor(type(self).__name__)
 
@@ -107,57 +97,56 @@ class BaseModel:
             if self.args_dict['log'] is not None:
                 self.save(full_log_path, self.args_dict['log'])
 
-            # validation sets could be too big, so we randomly choose some examples
-            n_validation = len(validation_x)
-            n_validation_examples = min(n_validation, 5000)
-            validation_indexes = np.random.choice(n_validation, size=n_validation_examples)
-            validation_x_sample = validation_x[:, validation_indexes]
-            validation_y_sample = validation_y[:, validation_indexes]
+            train_generator = train_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
+            validation_generator = validation_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
 
             step = self.sess.run(self.global_step)
             for epoch in range(epochs):
-                # shuffle indexes and then iterate over batches
-                batch_size = self.args_dict['batch_size']
-                n_train = len(train_x)
-                indexes = np.arange(n_train, dtype=np.int)
-                np.random.shuffle(indexes)
-
-                #######################
-                # Start Training Loop #
-                #######################
+                ############
+                # Training #
+                ############
 
                 self.train_init_epoch_hook(epoch, step)
 
-                for batch_start in range(0, n_train, batch_size):
-                    batch_indexes = indexes[batch_start:batch_start + batch_size]
-                    train_x_batch = train_x[:, batch_indexes]
-                    train_y_batch = train_y[:, batch_indexes]
+                for train_batch_x, train_batch_y in train_generator:
+                    train_feed_dict = self.build_feed_dict(train_batch_x, train_batch_y, **kwargs)
 
-                    train_feed_dict = self.build_feed_dict(train_x_batch, train_y_batch, **kwargs)
+                    self.train_feed_hook(step, train_batch_x, train_batch_y)
 
-                    self.train_feed_hook(step, train_x_batch, train_y_batch)
-
-                    step, train_summary, train_loss, _ = self.sess.run(train_ops, feed_dict=train_feed_dict)
+                    step, train_summary, train_batch_loss, _ = self.sess.run(train_ops, feed_dict=train_feed_dict)
 
                     if step % self.args_dict['log_period'] == 0 or step == 1:
                         if self.args_dict['log'] is not None:
                             writer.add_summary(train_summary, step)
 
                     if step % self.args_dict['print_period'] == 0 or step == 1:
-                        print('epoch {:4d}, step: {:4d}, train loss: {:8.4f}'.format(epoch, step, train_loss))
+                        print('epoch {:4d}, step: {:4d}, batch loss: {:8.4f}'.format(epoch, step, train_batch_loss))
 
-                #####################
-                # End Training Loop #
-                #####################
+                ##############
+                # Validation #
+                ##############
 
                 if epoch % self.args_dict['val_period'] == 0 or epoch == 0:
+
                     self.validation_init_hook(epoch, step)
-                    validation_feed_dict = self.build_feed_dict(validation_x_sample, validation_y_sample, **kwargs)
-                    validation_summary, validation_loss = self.sess.run(validation_ops, feed_dict=validation_feed_dict)
-                    validation_log_msg = 'epoch {:4d}, step: {:4d}, validation loss: {:8.4f}'.format(epoch, step, validation_loss)
+
+                    val_loss_sum = 0
+                    for validation_batch_x, validation_batch_y in validation_generator:
+                        validation_feed_dict = self.build_feed_dict(validation_batch_x, validation_batch_y, **kwargs)
+                        validation_summary, validation_loss = self.sess.run(validation_ops, feed_dict=validation_feed_dict)
+                        val_loss_sum += validation_loss
+
+                        if self.args_dict['log'] is not None:
+                            writer.add_summary(validation_summary, step)
+
+                    val_loss_mean = val_loss_sum / len(validation_generator)
+
+                    validation_log_msg = 'epoch {:4d}, step: {:4d} loss: {:8.4f}'.format(epoch, step, val_loss_mean)
                     print(Fore.YELLOW + validation_log_msg + Fore.RESET)
-                    if self.args_dict['log'] is not None:
-                        writer.add_summary(validation_summary, step)
+
+                ##########
+                # Saving #
+                ##########
 
                 if epoch % self.args_dict['save_period'] == 0 and epoch > 0:
                     if self.args_dict['log'] is not None:

link_bot_models/src/link_bot_models/constraint_cnn.py
@@ -4,15 +4,13 @@ import json
 import os
 
 import keras
-import numpy as np
 import tensorflow as tf
 from colorama import Fore
 from keras.backend.tensorflow_backend import set_session
-from keras.layers import Input, Dense, Conv2D, Flatten
+from keras.layers import Input, Dense, Conv2D, Flatten, MaxPool2D
 from keras.models import Model
 from keras.models import load_model
 
-from link_bot_models.label_types import LabelType
 from link_bot_pycommon import experiments_util
 
 
@@ -22,14 +20,6 @@ class ConstraintCNN:
         self.args_dict = args_dict
         self.N = N
 
-        self.label_type = self.args_dict['label_type']
-        if self.label_type == LabelType.SDF:
-            self.label_mask = np.array([1, 0], dtype=np.int)
-        elif self.label_type == LabelType.Overstretching:
-            self.label_mask = np.array([0, 1], dtype=np.int)
-        elif self.label_type == LabelType.SDF_and_Overstretching:
-            self.label_mask = np.array([1, 1], dtype=np.int)
-
         config = tf.ConfigProto()
         config.gpu_options.per_process_gpu_memory_fraction = 0.3
         set_session(tf.Session(config=config))
@@ -38,20 +28,21 @@ class ConstraintCNN:
         rope_input = Input(shape=(N,), dtype='float32', name='rope_configuration')
 
         self.conv_filters = [
-            (16, (3, 3)),
-            (16, (3, 3)),
+            (64, (3, 3)),
+            (64, (3, 3)),
         ]
 
         self.fc_layer_sizes = [
-            16,
-            8,
+            128,
+            128,
         ]
 
         conv_h = sdf_input
         for conv_filter in self.conv_filters:
             n_filters = conv_filter[0]
             filter_size = conv_filter[1]
-            conv_h = Conv2D(n_filters, filter_size)(conv_h)
+            conv_z = Conv2D(n_filters, filter_size)(conv_h)
+            conv_h = MaxPool2D(2)(conv_z)
         conv_output = Flatten()(conv_h)
 
         concat = keras.layers.concatenate([conv_output, rope_input])
@@ -65,7 +56,7 @@ class ConstraintCNN:
         self.keras_model = Model(inputs=[sdf_input, rope_input], outputs=predictions)
         self.keras_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    def metadata(self):
+    def metadata(self, label_types):
         metadata = {
             'tf_version': str(tf.__version__),
             'keras_version': str(keras.__version__),
@@ -73,13 +64,13 @@ class ConstraintCNN:
             'checkpoint': self.args_dict['checkpoint'],
             'N': self.N,
             'conv_filters': self.conv_filters,
-            'label_type': str(self.label_type),
+            'label_type': [label_type.name for label_type in label_types],
             'fc_layer_sizes': self.fc_layer_sizes,
             'commandline': self.args_dict['commandline'],
         }
         return metadata
 
-    def train(self, train_dataset, validation_dataset, epochs, log_path):
+    def train(self, train_dataset, validation_dataset, label_types, epochs, log_path):
         """
         Wrapper around model.fit
 
@@ -97,7 +88,7 @@ class ConstraintCNN:
 
             metadata_path = os.path.join(full_log_path, "metadata.json")
             metadata_file = open(metadata_path, 'w')
-            metadata = self.metadata()
+            metadata = self.metadata(label_types)
             metadata['log path'] = full_log_path
             metadata_file.write(json.dumps(metadata, indent=2))
 
@@ -108,20 +99,18 @@ class ConstraintCNN:
                                                                   period=1)
             callbacks.append(checkpoint_callback)
 
-        self.keras_model.fit_generator(train_dataset.generator(self.prepare_data, self.args_dict['batch_size']),
-                                       callbacks=callbacks,
-                                       validation_data=validation_dataset.generator(self.args_dict['batch_size']),
-                                       workers=4,
-                                       epochs=epochs)
+        train_generator = train_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
+        validation_generator = validation_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
+        self.keras_model.fit_generator(train_generator, callbacks=callbacks, validation_data=validation_generator, epochs=epochs)
         self.evaluate(validation_dataset)
 
-    def evaluate(self, eval_x, eval_y, display=True):
-        inputs, labels = self.prepare_data(eval_x, eval_y)
-        loss, accuracy = self.keras_model.evaluate(inputs, labels)
+    def evaluate(self, validation_dataset, label_types, display=True):
+        generator = validation_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
+        loss, accuracy = self.keras_model.evaluate_generator(generator)
 
         if display:
             print("Overall Loss: {:0.3f}".format(float(loss)))
-            print("constraint prediction accuracy:\n{}".format(accuracy))
+        print("constraint prediction accuracy:\n{}".format(accuracy))
 
         return loss, accuracy
 

link_bot_models/src/link_bot_models/constraint_sdf.py
@@ -2,13 +2,12 @@ from __future__ import division, print_function, absolute_import
 
 import matplotlib.pyplot as plt
 import numpy as np
-from colorama import Style
 import tensorflow as tf
 from attr import dataclass
+from colorama import Style
 
 from link_bot_models import plotting
 from link_bot_models.base_model import BaseModel
-from link_bot_models.label_types import LabelType
 from link_bot_models.tf_signed_distance_field_op import sdf_func
 from link_bot_pycommon import link_bot_pycommon
 
@@ -17,14 +16,6 @@ class ConstraintSDF(BaseModel):
 
     def __init__(self, args, sdf_shape, N):
         super(ConstraintSDF, self).__init__(args, N)
-
-        self.label_type = self.args_dict['label_type']
-        if self.label_type == LabelType.SDF:
-            self.label_mask = np.array([1, 0], dtype=np.int)
-        elif self.label_type == LabelType.Overstretching:
-            self.label_mask = np.array([0, 1], dtype=np.int)
-        elif self.label_type == LabelType.SDF_and_Overstretching:
-            self.label_mask = np.array([1, 1], dtype=np.int)
 
         self.beta = 1e-2
 
@@ -35,9 +26,8 @@ class ConstraintSDF(BaseModel):
         self.sdf_resolution = tf.placeholder(tf.float32, shape=[batch_dim, 2], name="sdf_resolution")
         self.sdf_extent = tf.placeholder(tf.float32, shape=[batch_dim, 4], name="sdf_extent")
 
-        self.observations = tf.placeholder(tf.float32, shape=[batch_dim, N], name="observations")
-        self.k_label = tf.placeholder(tf.float32, shape=[batch_dim, 1], name="k")
-        self.k_label_int = tf.cast(self.k_label, tf.int32)
+        self.rope_configuration = tf.placeholder(tf.float32, shape=[batch_dim, N], name="rope_configuration")
+        self.label = tf.placeholder(tf.float32, shape=[batch_dim, 1], name="label")
         self.hidden_layer_dims = None
         self.fig = None
 
@@ -48,102 +38,81 @@ class ConstraintSDF(BaseModel):
         k_threshold_init = 0.0
         self.threshold_k = tf.get_variable("threshold_k", initializer=k_threshold_init, trainable=False)
         self.hidden_layer_dims = [6]
-        h = self.observations
+        h = self.rope_configuration
         for layer_idx, hidden_layer_dim in enumerate(self.hidden_layer_dims):
             h = tf.layers.dense(h, hidden_layer_dim, activation=tf.nn.relu, use_bias=False,
                                 name='hidden_layer_{}'.format(layer_idx))
-        self.hat_latent_k = tf.layers.dense(h, 2, activation=None, use_bias=True, name='output_layer')
+        self.sdf_input = tf.layers.dense(h, 2, activation=None, use_bias=False, name='output_layer')
 
         #######################################################
         #                 End Model Definition                #
         #######################################################
 
-        self.sdfs = sdf_func(self.sdf, self.sdf_gradient, self.sdf_resolution, self.sdf_origin, self.hat_latent_k, 2)
+        self.sdfs = sdf_func(self.sdf, self.sdf_gradient, self.sdf_resolution, self.sdf_origin, self.sdf_input, 2)
         self.sigmoid_scale = 1.0
-        self.hat_k = self.sigmoid_scale * (self.threshold_k - self.sdfs)
+        self.prediction = self.sigmoid_scale * (self.threshold_k - self.sdfs)
         self.hat_k_violated = tf.cast(self.sdfs < self.threshold_k, dtype=tf.int32, name="hat_k_violated")
-        _, self.constraint_prediction_accuracy = tf.metrics.accuracy(labels=self.k_label,
-                                                                     predictions=self.hat_k_violated,
-                                                                     name="constraint_prediction_accuracy_metric")
-        _, self.constraint_prediction_precision = tf.metrics.precision(labels=self.k_label,
-                                                                       predictions=self.hat_k_violated,
-                                                                       name="constraint_prediction_precision_metric")
-        _, self.constraint_prediction_recall = tf.metrics.recall(labels=self.k_label,
-                                                                 predictions=self.hat_k_violated,
-                                                                 name="constraint_prediction_recall_metric")
-        self.accuracy_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="constraint_prediction_accuracy_metric")
+        self.get_accuracy, self.update_accuracy = tf.metrics.accuracy(labels=self.label, predictions=self.hat_k_violated,
+                                                                      name="accuracy_metric")
+        self.get_precision, self.update_precision = tf.metrics.precision(labels=self.label, predictions=self.hat_k_violated,
+                                                                         name="precision_metric")
+        self.get_recall, self.update_recall = tf.metrics.recall(labels=self.label, predictions=self.hat_k_violated,
+                                                                name="recall_metric")
+        self.accuracy_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="accuracy_metric")
         self.accuracy_local_vars_initializer = tf.variables_initializer(var_list=self.accuracy_local_vars)
-        self.precision_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES,
-                                                      scope="constraint_prediction_precision_metric")
+        self.precision_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="precision_metric")
         self.precision_local_vars_initializer = tf.variables_initializer(var_list=self.precision_local_vars)
-        self.recall_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="constraint_prediction_recall_metric")
+        self.recall_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="recall_metric")
         self.recall_local_vars_initializer = tf.variables_initializer(var_list=self.recall_local_vars)
 
         with tf.name_scope("train"):
             # sum of squared errors in latent space at each time step
-            self.constraint_prediction_error = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.hat_k,
-                                                                                       labels=self.k_label,
-                                                                                       name='constraint_prediction_err')
-            self.constraint_prediction_loss = tf.reduce_mean(self.constraint_prediction_error,
-                                                             name="constraint_prediction_loss")
+            self.per_example_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.prediction, labels=self.label)
+            self.prediction_loss = tf.reduce_mean(self.per_example_loss, name="prediction_loss")
 
             # FIXME: this assumes that the physical world coordinates (0,0) in meters is the origin/center of the SDF
-            self.distances_to_origin = tf.norm(self.hat_latent_k, axis=1)
-            oob_left = self.hat_latent_k[:, 0] <= self.sdf_extent[:, 0]
-            oob_right = self.hat_latent_k[:, 0] >= self.sdf_extent[:, 1]
-            oob_up = self.hat_latent_k[:, 1] <= self.sdf_extent[:, 2]
-            oob_down = self.hat_latent_k[:, 1] >= self.sdf_extent[:, 3]
+            self.distances_to_origin = tf.norm(self.sdf_input, axis=1)
+            oob_left = self.sdf_input[:, 0] <= self.sdf_extent[:, 0]
+            oob_right = self.sdf_input[:, 0] >= self.sdf_extent[:, 1]
+            oob_up = self.sdf_input[:, 1] <= self.sdf_extent[:, 2]
+            oob_down = self.sdf_input[:, 1] >= self.sdf_extent[:, 3]
             self.out_of_bounds = tf.math.reduce_any(tf.stack((oob_up, oob_down, oob_left, oob_right), axis=1), axis=1, name='oob')
             self.in_bounds_value = tf.ones_like(self.distances_to_origin) * 0.0
             self.distances_out_of_bounds = tf.where(self.out_of_bounds, self.distances_to_origin, self.in_bounds_value)
             self.out_of_bounds_loss = tf.reduce_mean(self.distances_out_of_bounds, name='out_of_bounds_loss')
 
             self.loss = tf.add_n([
-                self.constraint_prediction_loss,
+                self.prediction_loss,
                 self.beta * self.out_of_bounds_loss,
             ], name='loss')
 
             self.global_step = tf.get_variable("global_step", initializer=0, trainable=False)
             self.opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss, global_step=self.global_step)
 
-            trainable_vars = tf.trainable_variables()
-            for var in trainable_vars:
-                name = var.name.replace(":", "_")
-                grads = tf.gradients(self.loss, var, name='dLoss_d{}'.format(name))
-                for grad in grads:
-                    if grad is not None:
-                        tf.summary.histogram(name + "/gradient", grad, collections=['train'])
-                    else:
-                        print("Warning... there is no gradient of the loss with respect to {}".format(var.name))
+            # trainable_vars = tf.trainable_variables()
+            # for var in trainable_vars:
+            #     name = var.name.replace(":", "_")
+            #     grads = tf.gradients(self.loss, var, name='dLoss_d{}'.format(name))
+            #     for grad in grads:
+            #         if grad is not None:
+            #             tf.summary.histogram(name + "/gradient", grad, collections=['train', 'validation'])
+            #         else:
+            #             print("Warning... there is no gradient of the loss with respect to {}".format(var.name))
 
-            tf.summary.scalar("constraint_prediction_accuracy_summary", self.constraint_prediction_accuracy,
-                              collections=['train'])
-            tf.summary.scalar("constraint_prediction_precision_summary", self.constraint_prediction_precision,
-                              collections=['train'])
-            tf.summary.scalar("constraint_prediction_recall_summary", self.constraint_prediction_recall,
-                              collections=['train'])
-            # tf.summary.scalar("k_threshold_summary", self.threshold_k, collections=['train'])
-            tf.summary.scalar("out_of_bounds_loss_summary", self.out_of_bounds_loss,
-                              collections=['train'])
-            tf.summary.scalar("constraint_prediction_loss_summary", self.constraint_prediction_loss,
-                              collections=['train'])
-            tf.summary.scalar("loss_summary", self.loss,
-                              collections=['train'])
+            tf.summary.scalar("constraint_prediction_accuracy_summary", self.update_accuracy, collections=['train'])
+            tf.summary.scalar("constraint_prediction_precision_summary", self.update_precision, collections=['train'])
+            tf.summary.scalar("constraint_prediction_recall_summary", self.update_recall, collections=['train'])
+            tf.summary.scalar("out_of_bounds_loss_summary", self.out_of_bounds_loss, collections=['train'])
+            tf.summary.scalar("constraint_prediction_loss_summary", self.prediction_loss, collections=['train'])
+            tf.summary.scalar("loss_summary", self.loss, collections=['train'])
 
         with tf.name_scope("validation"):
-            tf.summary.scalar("constraint_prediction_accuracy_summary", self.constraint_prediction_accuracy,
-                              collections=['validation'])
-            tf.summary.scalar("constraint_prediction_precision_summary", self.constraint_prediction_precision,
-                              collections=['validation'])
-            tf.summary.scalar("constraint_prediction_recall_summary", self.constraint_prediction_recall,
-                              collections=['validation'])
-            # tf.summary.scalar("k_threshold_summary", self.threshold_k, collections=['validation'])
-            tf.summary.scalar("out_of_bounds_loss_summary", self.out_of_bounds_loss,
-                              collections=['validation'])
-            tf.summary.scalar("constraint_prediction_loss_summary", self.constraint_prediction_loss,
-                              collections=['validation'])
-            tf.summary.scalar("loss_summary", self.loss,
-                              collections=['validation'])
+            tf.summary.scalar("constraint_prediction_accuracy_summary", self.update_accuracy, collections=['validation'])
+            tf.summary.scalar("constraint_prediction_precision_summary", self.update_precision, collections=['validation'])
+            tf.summary.scalar("constraint_prediction_recall_summary", self.update_recall, collections=['validation'])
+            tf.summary.scalar("out_of_bounds_loss_summary", self.out_of_bounds_loss, collections=['validation'])
+            tf.summary.scalar("constraint_prediction_loss_summary", self.prediction_loss, collections=['validation'])
+            tf.summary.scalar("loss_summary", self.loss, collections=['validation'])
 
         self.finish_setup()
 
@@ -155,42 +124,51 @@ class ConstraintSDF(BaseModel):
             'N': self.N,
             'beta': self.beta,
             'sigmoid_scale': self.sigmoid_scale,
-            'label_type': str(self.label_type),
+            'label_type': 'TODO: implement me',
             'commandline': self.args_dict['commandline'],
             'hidden_layer_dims': self.hidden_layer_dims,
         }
         return metadata
 
     def build_feed_dict(self, x, y: np.ndarray, **kwargs):
-        """
-        :param x: first dim is type of input, second dim is batch, following dims are data
-        :param y: first dim is type of label, second dim is batch, following dims are labels
-        :param kwargs:
-        :return:
-        """
-        labels = np.any(y[0] * self.label_mask, axis=1, keepdims=True).astype(np.float32)
-        return {self.observations: x[0],
-                self.sdf: x[1],
-                self.sdf_gradient: x[2],
-                self.sdf_origin: x[3],
-                self.sdf_resolution: x[4],
-                self.sdf_extent: x[5],
-                self.k_label: labels}
+        return {self.rope_configuration: x['rope_configuration'],
+                self.sdf: np.squeeze(x['sdf']),
+                self.sdf_gradient: x['sdf_gradient'],
+                self.sdf_origin: x['sdf_origin'],
+                self.sdf_resolution: x['sdf_resolution'],
+                self.sdf_extent: x['sdf_extent'],
+                self.label: y['combined_output']}
 
-    def evaluate(self, eval_x, eval_y, display=True):
-        feed_dict = self.build_feed_dict(eval_x, eval_y)
-        ops = [self.threshold_k, self.constraint_prediction_loss, self.loss, self.constraint_prediction_accuracy,
-               self.constraint_prediction_precision, self.constraint_prediction_recall]
-        threshold, k_loss, loss, accuracy, precision, recall = self.sess.run(ops, feed_dict=feed_dict)
+    def evaluate(self, eval_dataset, label_types, display=True):
+        generator = eval_dataset.generator_specific_labels(label_types, self.args_dict['batch_size'])
+
+        k_loss_sum = 0
+        loss_sum = 0
+        for eval_batch_x, eval_batch_y in generator:
+            feed_dict = self.build_feed_dict(eval_batch_x, eval_batch_y)
+            ops = [self.hat_k_violated, self.prediction_loss, self.loss, self.update_accuracy,
+                   self.update_precision, self.update_recall]
+            hat_k_violated, k_loss, loss, _, _, _ = self.sess.run(ops, feed_dict=feed_dict)
+            k_loss_sum += k_loss
+            loss_sum += loss
+
+        m = len(generator)
+        k_loss_mean = k_loss_sum / m
+        loss_mean = loss_sum / m
+
+        # The metrics have now been updated over the entire eval dataset,
+        # so this essentially just computes the final metric values
+        metrics_ops = [self.get_accuracy, self.get_precision, self.get_recall]
+        accuracy, precision, recall = self.sess.run(metrics_ops)
 
         if display:
-            print("Constraint Loss: {:0.3f}".format(float(k_loss)))
-            print("Overall Loss: {:0.3f}".format(float(loss)))
+            print("Constraint Loss: {:0.3f}".format(k_loss_mean))
+            print("Overall Loss: {:0.3f}".format(loss_mean))
             print("Precision: {:4.1f}%".format(precision * 100))
             print("Recall: {:4.1f}%".format(recall * 100))
             print(Style.BRIGHT + "Accuracy: {:4.1f}%".format(accuracy * 100) + Style.NORMAL)
 
-        return threshold, k_loss, loss
+        return k_loss_mean, loss_mean
 
     def start_train_hook(self):
         self.fig = plt.figure()
@@ -198,10 +176,14 @@ class ConstraintSDF(BaseModel):
     def train_init_epoch_hook(self, epoch, step):
         # reset at the start of each epoch we are computing accuracy for the training set but not all training history
         self.sess.run(self.accuracy_local_vars_initializer)
+        self.sess.run(self.precision_local_vars_initializer)
+        self.sess.run(self.recall_local_vars_initializer)
 
     def validation_init_hook(self, epoch, step):
         # reset at the start of validation so we clear the metrics from training steps
         self.sess.run(self.accuracy_local_vars_initializer)
+        self.sess.run(self.precision_local_vars_initializer)
+        self.sess.run(self.recall_local_vars_initializer)
 
     def train_feed_hook(self, iteration, train_x_batch, train_y_batch):
         if 'plot_gradient_descent' in self.args_dict:
@@ -220,13 +202,13 @@ class ConstraintSDF(BaseModel):
         sdf_resolutions = np.tile(sdf_data.resolution, [n_observations, 1])
         sdf_extents = np.tile(sdf_data.extent, [n_observations, 1])
         feed_dict = {
-            self.observations: observations,
+            self.rope_configuration: observations,
             self.sdf: sdfs,
             self.sdf_origin: sdf_origins,
             self.sdf_resolution: sdf_resolutions,
             self.sdf_extent: sdf_extents,
         }
-        predicted_violated, pt = self.sess.run([self.hat_k_violated, self.hat_latent_k], feed_dict=feed_dict)
+        predicted_violated, pt = self.sess.run([self.hat_k_violated, self.sdf_input], feed_dict=feed_dict)
         return np.any(predicted_violated, axis=1), pt
 
     def __str__(self):

link_bot_models/src/link_bot_models/multi_environment_datasets.py
@@ -1,75 +1,11 @@
 import json
 import os
+import keras
 
 import numpy as np
 
 from link_bot_models.label_types import LabelType
 from link_bot_pycommon.link_bot_pycommon import SDF
-
-
-class FancyList:
-
-    def __init__(self, items):
-        self.items = items
-
-    def __len__(self):
-        return self.items[0].shape[0]
-
-    def __getitem__(self, index):
-        if isinstance(index, int):
-            return self.items[index]
-        elif isinstance(index, list):
-            return [self.items[i] for i in index]
-        elif isinstance(index, slice):
-            return self.items[index]
-        elif isinstance(index, tuple):
-            if isinstance(index[0], slice) and isinstance(index[1], int):
-                return [item[index[1]] for item in self.items[index[0]]]
-            elif isinstance(index[0], slice) and isinstance(index[1], list):
-                return [[item[j] for j in index[1]] for item in self.items[index[0]]]
-            elif isinstance(index[0], slice) and isinstance(index[1], np.ndarray) and len(index[1].shape) == 1:
-                return [[item[j] for j in index[1]] for item in self.items[index[0]]]
-            else:
-                raise ValueError("unsupported types for index: (" + ', '.join([str(type(i)) for i in index]) + ")")
-        else:
-            raise ValueError("unsupported type for index {}".format(type(index)))
-
-
-def make_inputs_and_labels(environments):
-    """ merges all the data and labels from the environments into a format suitable for batching into feed dicts """
-    sdfs = []
-    sdf_gradients = []
-    sdf_origins = []
-    sdf_resolutions = []
-    sdf_extents = []
-    rope_configurations = []
-    constraint_labels = []
-    for environment in environments:
-        # environment is a multi_environment_datasets.Environment
-        env_rope_configurations = environment.rope_data['rope_configurations']
-        env_constraint_labels = environment.rope_data['constraints']
-        for rope_configuration, constraint_label in zip(env_rope_configurations, env_constraint_labels):
-            sdfs.append(environment.sdf_data.sdf)
-            sdf_gradients.append(environment.sdf_data.gradient)
-            sdf_origins.append(environment.sdf_data.origin)
-            sdf_resolutions.append(environment.sdf_data.resolution)
-            sdf_extents.append(environment.sdf_data.extent)
-            rope_configurations.append(rope_configuration)
-            constraint_labels.append(constraint_label)
-
-    inputs = FancyList([
-        np.array(rope_configurations),
-        np.array(sdfs),
-        np.array(sdf_gradients),
-        np.array(sdf_origins),
-        np.array(sdf_resolutions),
-        np.array(sdf_extents),
-    ])
-    labels = FancyList([
-        np.array(constraint_labels),
-    ])
-
-    return inputs, labels
 
 
 class EnvironmentData:
@@ -97,9 +33,10 @@ class MultiEnvironmentDataset:
         self.example_information = []
         self.sdf_shape = None
         self.n_environments = len(filename_pairs)
+        self.environments = np.ndarray([self.n_environments], dtype=np.object)
         example_id = 0
-        for i, (sdf_filename, rope_data_filename) in enumerate(filename_pairs):
-            sdf_data = SDF.load(sdf_filename)
+        for i, (sdf_data_filename, rope_data_filename) in enumerate(filename_pairs):
+            sdf_data = SDF.load(sdf_data_filename)
             rope_data = np.load(rope_data_filename)
             examples_in_env = rope_data['rope_configurations'].shape[0]
 
@@ -111,9 +48,12 @@ class MultiEnvironmentDataset:
             error_msg = "SDFs shape {} doesn't match shape {}".format(self.sdf_shape, sdf_data.sdf.shape)
             assert self.sdf_shape == sdf_data.sdf.shape, error_msg
 
+            env = EnvironmentData(sdf_data, rope_data)
+            self.environments[i] = env
+
             for j in range(examples_in_env):
                 self.example_information.append({
-                    'sdf_filename': sdf_filename,
+                    'sdf_data_filename': sdf_data_filename,
                     'rope_data_filename': rope_data_filename,
                     'rope_data_index': j
                 })
@@ -121,7 +61,11 @@ class MultiEnvironmentDataset:
         self.num_examples = example_id
 
     def generator(self, batch_size):
-        return DatasetGenerator(self, self.constraint_label_types, batch_size)
+        return self.generator_specific_labels(self.constraint_label_types, batch_size)
+
+    def generator_specific_labels(self, constraint_label_types, batch_size):
+        """ allows you to test with just some of the labels """
+        return DatasetGenerator(self, constraint_label_types, batch_size)
 
     @staticmethod
     def load_dataset(dataset_filename):
@@ -152,14 +96,21 @@ class MultiEnvironmentDataset:
         }
         json.dump(dataset_dict, open(dataset_filename, 'w'), sort_keys=True, indent=4)
 
+    def __len__(self):
+        return self.num_examples
 
-class DatasetGenerator:
 
-    def __init__(self, dataset, label_types, batch_size):
+class DatasetGenerator(keras.utils.Sequence):
+
+    def __init__(self, dataset, label_types, batch_size, shuffle=True):
+        batch_size_err_msg = "Batch size {} must even divide the dataset size {}".format(batch_size, len(dataset))
+        assert len(dataset) % batch_size == 0, batch_size_err_msg
+
         self.dataset = dataset
         self.batch_size = batch_size
         examples_ids = np.arange(0, self.dataset.num_examples)
-        np.random.shuffle(examples_ids)
+        if shuffle:
+            np.random.shuffle(examples_ids)
         self.batches = np.reshape(examples_ids, [-1, batch_size])
         self.label_mask = LabelType.mask(label_types)
 
@@ -174,7 +125,7 @@ class DatasetGenerator:
         """
         batch_indeces = self.batches[index]
         x = {
-            'sdf': np.ndarray([self.batch_size, self.dataset.sdf_shape[0], self.dataset.sdf_shape[1]]),
+            'sdf': np.ndarray([self.batch_size, self.dataset.sdf_shape[0], self.dataset.sdf_shape[1], 1]),
             'sdf_gradient': np.ndarray([self.batch_size, self.dataset.sdf_shape[0], self.dataset.sdf_shape[1], 2]),
             'sdf_origin': np.ndarray([self.batch_size, 2]),
             'sdf_resolution': np.ndarray([self.batch_size, 2]),
@@ -186,17 +137,31 @@ class DatasetGenerator:
             'all_output': np.ndarray([self.batch_size, self.label_mask.shape[0]]),
         }
 
+        loaded_data_cache = {}
         for i, example_id in enumerate(batch_indeces):
             example_info = self.dataset.example_information[example_id]
-            sdf_data = SDF.load(example_info['sdf_filename'])
-            rope_data = np.load(example_info['rope_data_filename'])
+
+            sdf_data_filename = example_info['sdf_data_filename']
+            rope_data_filename = example_info['rope_data_filename']
+
+            if sdf_data_filename in loaded_data_cache:
+                sdf_data = loaded_data_cache[sdf_data_filename]
+            else:
+                sdf_data = SDF.load(sdf_data_filename)
+                loaded_data_cache[sdf_data_filename] = sdf_data
+
+            if rope_data_filename in loaded_data_cache:
+                rope_data = loaded_data_cache[rope_data_filename]
+            else:
+                rope_data = np.load(rope_data_filename)
+                loaded_data_cache[rope_data_filename] = rope_data
 
             rope_configuration = rope_data['rope_configurations'][example_info['rope_data_index']]
 
             all_label = rope_data['constraints'][example_info['rope_data_index']].astype(np.float32)
             combined_label = np.any(all_label * self.label_mask).astype(np.float32)
 
-            x['sdf'][i] = sdf_data.sdf
+            x['sdf'][i] = np.atleast_3d(sdf_data.sdf)
             x['sdf_gradient'][i] = sdf_data.gradient
             x['sdf_origin'][i] = sdf_data.origin
             x['sdf_resolution'][i] = sdf_data.resolution