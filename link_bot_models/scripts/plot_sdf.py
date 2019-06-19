3b3f603ddc5d5620d5cd30163758b5479762f0d1
Peter Mitrano <pmitrano@umich.edu>
2019-06-19 11:36:15 -0400

    simple plotting script

---
+++ link_bot_models/scripts/plot_sdf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+from link_bot_pycommon.link_bot_pycommon import SDF
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sdf_files', nargs='+')
+    parser.add_argument('--threshold', type=float, default=0.0)
+
+    args = parser.parse_args()
+
+    for sdf_file in args.sdf_files:
+        sdf_data = SDF.load(sdf_file)
+
+        binary_image = np.asarray(sdf_data.image) > args.threshold
+        plt.figure()
+        plt.title(sdf_file)
+        plt.imshow(binary_image, extent=sdf_data.extent)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
