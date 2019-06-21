c0ba8e69b8dcab6d823d468aa2caf533be418c23
Peter Mitrano <pmitrano@umich.edu>
2019-06-21 13:26:09 -0400

    tried a dumb MLP w/ ReLUs to predict overstretching
    doesn't work at all, which isn't really surprising
    normally one would do some feature engineering here

---
+++ link_bot_models/src/link_bot_models/relu_model.py
@@ -0,0 +1,64 @@
+from __future__ import division, print_function, absolute_import
+
+import keras
+import numpy as np
+import tensorflow as tf
+from keras.layers import Input, Dense, Activation, Lambda
+from keras.models import Model
+
+from link_bot_models.base_model import BaseModel
+
+
+class ReluModel(BaseModel):
+
+    def __init__(self, args_dict, N):
+        super(ReluModel, self).__init__(args_dict, N)
+
+        rope_input = Input(shape=(N,), dtype='float32', name='rope_configuration')
+
+        self.fc_layer_sizes = [
+            1024,
+            1024,
+            1024,
+            1024,
+        ]
+
+        fc_h = rope_input
+        for fc_layer_size in self.fc_layer_sizes:
+            fc_h = Dense(fc_layer_size, activation='relu')(fc_h)
+        logits = Dense(1, activation=None)(fc_h)
+        sigmoid_scale = args_dict['sigmoid_scale']
+        scaled_logits = Lambda(lambda x: sigmoid_scale * x)(logits)
+        predictions = Activation('sigmoid', name='combined_output')(scaled_logits)
+
+        # This creates a model that includes
+        self.model_inputs = [rope_input]
+        self.keras_model = Model(inputs=self.model_inputs, outputs=predictions)
+        self.keras_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def metadata(self, label_types):
+        metadata = {
+            'tf_version': str(tf.__version__),
+            'keras_version': str(keras.__version__),
+            'seed': self.args_dict['seed'],
+            'checkpoint': self.args_dict['checkpoint'],
+            'N': self.N,
+            'label_type': [label_type.name for label_type in label_types],
+            'sigmoid_scale': self.args_dict['sigmoid_scale'],
+            'fc_layer_sizes': self.fc_layer_sizes,
+            'commandline': self.args_dict['commandline'],
+        }
+        return metadata
+
+    def violated(self, observations):
+        rope_configuration = observations
+        inputs_dict = {
+            'rope_configuration': rope_configuration,
+        }
+
+        predicted_violated = (self.keras_model.predict(inputs_dict) > 0.5).astype(np.bool)
+
+        return predicted_violated
+
+    def __str__(self):
+        return "keras relu model"
