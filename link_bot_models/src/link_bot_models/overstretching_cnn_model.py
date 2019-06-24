dce8d3dab268fd54dd2d2b8fbc3c7e8e66d5b1b9
Peter Mitrano (ARMNOVA) <pmitrano@umich.edu>
2019-06-24 11:37:14 -0400

    just can't get overstretching CNN to not overfit

--- link_bot_models/src/link_bot_models/overstretching_cnn_model.py
+++ link_bot_models/src/link_bot_models/overstretching_cnn_model.py
@@ -20,13 +20,11 @@ class OverstretchingCNNModel(BaseModel):
         rope_image = Input(shape=(sdf_shape[0], sdf_shape[1], 3), dtype='float32', name='rope_image')
 
         self.conv_filters = [
-            (16, (5, 5)),
-            (16, (3, 3)),
+            (16, (7, 7)),
         ]
 
         self.fc_layer_sizes = [
-            32,
-            32,
+            16,
         ]
 
         conv_h = rope_image
