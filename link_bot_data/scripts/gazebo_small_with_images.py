d09a13d3f57a3b4ac593fb04b8e5837c7ca8419b
Peter Mitrano <pmitrano@umich.edu>
2019-07-26 09:58:53 -0400

    fixing camera bug in data collection

--- link_bot_data/scripts/gazebo_small_with_images.py
+++ link_bot_data/scripts/gazebo_small_with_images.py
@@ -100,7 +100,7 @@ def generate_traj(args, services, env_idx):
         while True:
             s = services.get_state(state_req)
             image = np.frombuffer(s.camera_image.data, dtype=np.uint8)
-            if image.size == 64 * 64 * 3 and image.min() != image.max():
+            if image.size == 64 * 64 * 3 or image.min() != image.max():
                 break
             step = WorldControlRequest()
             step.steps = int(0.05 / 0.001)  # assuming 0.001s per simulation step
