9630d50a3d8dff6d75b76cc7c942325d7f795ea9
Peter Mitrano <pmitrano@umich.edu>
2019-08-07 17:04:55 -0400

    script for merging tfrecord folders works

--- link_bot_data/scripts/merge_tfrecords_datasets.py
+++ link_bot_data/scripts/merge_tfrecords_datasets.py
@@ -1,23 +1,50 @@
+#!/usr/bin/env python
+
 import argparse
 import glob
-from colorama import Fore
 import os
-import shutils
+import pathlib
+import re
+import shutil
+
+from colorama import Fore
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_arugment("indirs", nargs="?")
-    parser.add_argument("outdir")
+    parser.add_argument("indirs", nargs="*")
+    parser.add_argument("--outdir")
+    parser.add_argument("--dry-run", action='store_true')
 
     args = parser.parse_args()
 
-    for indir in args.indirs:
-        if not os.path.isdir(indir):
-            print(Fore.YELLOW + "{} is not a directory".format(indir) + Fore.RESET)
-        tfrecord_files = glob.glob(indir + "/*.tfrecords")
-        print(tfrecord_files)
+    if args.outdir and not os.path.isdir(args.outdir):
+        print(Fore.YELLOW + "{} is not a directory".format(args.outdir) + Fore.RESET)
+        return
+
+    files = []
+    for in_dir in args.indirs:
+        if not os.path.isdir(in_dir):
+            print(Fore.YELLOW + "{} is not a directory".format(in_dir) + Fore.RESET)
+            return
+        tfrecord_files = glob.glob(in_dir + "/*.tfrecords")
+        files.extend(tfrecord_files)
+
+    traj_idx = 0
+    for i, file in enumerate(files):
+        path = pathlib.Path(file)
+        parent = path.parent
+        filename = path.name
+        m = re.match(r"traj_(\d+)_to_(\d+).tfrecords", filename)
+        start, end = m.group(1), m.group(2)
+        n_trajs_in_file = int(end) - int(start)
+        new_filename = "traj_{}_to_{}.tfrecords".format(traj_idx, traj_idx + n_trajs_in_file)
+        new_path = pathlib.Path(args.outdir) / new_filename
+        traj_idx = traj_idx + n_trajs_in_file + 1
+        print(path, '-->',  new_path)
+        if not args.dry_run:
+            shutil.copyfile(path, new_path)
 
 
 if __name__ == '__main__':
     main()
-
