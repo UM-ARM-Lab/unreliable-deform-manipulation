16c0230b64ed9877768d9a6fb2e938074d8289e2
Peter Mitrano <pmitrano@umich.edu>
2019-12-04 22:31:04 -0500

    finished comparison between latest classifiers
    to reproduce:
    ./scripts/analyze_compare_classifiers_results.py results/compare_classifiers_4a156599de_1575489355

--- link_bot_planning/scripts/analyze_compare_classifiers_results.py
+++ link_bot_planning/scripts/analyze_compare_classifiers_results.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import plotly.graph_objects as go
 import json
 import pathlib
 from typing import List, Dict
@@ -12,19 +13,18 @@ from scipy import stats
 from tabulate import tabulate
 
 from link_bot_pycommon.args import my_formatter
-from link_bot_pycommon.metric_utils import make_row
+from link_bot_pycommon.metric_utils import row_stats
 
 
-def dict_to_pvale_table(data_dict):
-    pvalues = np.zeros([len(data_dict), len(data_dict)])
+def dict_to_pvale_table(data_dict: Dict, table_format: str):
+    pvalues = np.zeros([len(data_dict), len(data_dict) + 1], dtype=object)
     for i, (name1, e1) in enumerate(data_dict.items()):
-        for j, (name2, e2) in enumerate(data_dict.items()):
+        pvalues[i, 0] = name1
+        for j, (_, e2) in enumerate(data_dict.items()):
             _, pvalue = stats.ttest_ind(e1, e2)
-            pvalues[i, j] = pvalue
+            pvalues[i, j + 1] = pvalue
     headers = [''] + list(data_dict.keys())
-    pvalues
-    table = tabulate(pvalues, headers=headers, tablefmt='github', floatfmt='6.4f')
-    # table[1, 0] = ''
+    table = tabulate(pvalues, headers=headers, tablefmt=table_format, floatfmt='6.4f')
     return table
 
 
@@ -38,21 +38,21 @@ def invert_dict(data: List) -> Dict:
     return d
 
 
-def error_metrics(planning_times, path_length, final_errors):
-    return np.array([make_row('planning time (s)', planning_times),
-                     make_row('path length (m)', path_length),
-                     make_row('final error (m)', final_errors),
-                     ], dtype=np.object)
-
-
 def main():
     parser = argparse.ArgumentParser(formatter_class=my_formatter)
     parser.add_argument('results_dir', help='foldering containing folders containing metrics.jsons files', type=pathlib.Path)
+    parser.add_argument('--table-format', choices=['latex', 'github'], default='github', type=str, help='printing format')
 
     args = parser.parse_args()
 
     subfolders = args.results_dir.iterdir()
     final_errors_comparisons = {}
+    aggregate_metrics = {
+        'planning time': [['min', 'max', 'mean', 'median', 'std']],
+        'final tail error': [['min', 'max', 'mean', 'median', 'std']],
+    }
+
+    headers = ['']
     for subfolder in subfolders:
         if not subfolder.is_dir():
             continue
@@ -62,21 +62,29 @@ def main():
 
         data = invert_dict(data)
         planning_times = data['planning_time']
-        path_length = data['path_length']
         final_errors = data['final_execution_error']
         final_errors_comparisons[str(subfolder.name)] = final_errors
+        headers.append(str(subfolder.name))
 
-        headers = ['error metric', 'min', 'max', 'mean', 'median', 'std']
-        aggregate_metrics = error_metrics(planning_times, path_length, final_errors)
-        table = tabulate(aggregate_metrics, headers=headers, tablefmt='github', floatfmt='6.4f')
-        print(Style.BRIGHT + "{}:".format(subfolder.name) + Style.RESET_ALL)
-        print(table)
+        aggregate_metrics['planning time'].append(row_stats(planning_times))
+        aggregate_metrics['final tail error'].append(row_stats(final_errors))
 
         plt.title(subfolder.name)
         plt.hist(final_errors)
 
-    print(Style.BRIGHT + "n_parallel_calls-value matrix" + Style.RESET_ALL)
-    print(dict_to_pvale_table(final_errors_comparisons))
+    for metric_name, table_data in aggregate_metrics.items():
+        data = [go.Table(name=metric_name,
+                         header={'values': headers,
+                                 'font_size': 18},
+                         cells={'values': table_data,
+                                'format': [None, '5.3f', '5.3f', '5.3f'],
+                                'font_size': 14})]
+        fig = go.Figure(data)
+        outfile = args.results_dir / '{}_table.png'.format(metric_name)
+        fig.write_image(str(outfile), scale=4)
+
+    print(Style.BRIGHT + "p-value matrix" + Style.RESET_ALL)
+    print(dict_to_pvale_table(final_errors_comparisons, table_format=args.table_format))
 
 
 if __name__ == '__main__':
--- link_bot_pycommon/src/link_bot_pycommon/metric_utils.py
+++ link_bot_pycommon/src/link_bot_pycommon/metric_utils.py
@@ -3,3 +3,6 @@ import numpy as np
 
 def make_row(metric_name, e):
     return [metric_name, np.min(e), np.max(e), np.mean(e), np.median(e), np.std(e)]
+
+def row_stats(e):
+    return [np.min(e), np.max(e), np.mean(e), np.median(e), np.std(e)]
