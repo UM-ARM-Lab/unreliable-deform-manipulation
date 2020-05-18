b0bd64a6351374a703565c1c01793b1d057daa49
Peter Mitrano <pmitrano@umich.edu>
2020-05-18 17:49:12 -0400

    debugging out of ram issues

--- link_bot_data/src/link_bot_data/tests/test_dataset_performance.py
+++ link_bot_data/src/link_bot_data/tests/test_dataset_performance.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import argparse
 import pathlib
+import os
+import matplotlib.pyplot as plt
+import psutil
 from time import perf_counter
 
 import progressbar
@@ -13,39 +16,38 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=my_formatter)
     parser.add_argument('dataset_dir', type=pathlib.Path, help='dataset directory', nargs='+')
     parser.add_argument('--mode', choices=['train', 'test', 'val'], default='train')
-    parser.add_argument('--n-repetitions', type=int, default=1)
+    parser.add_argument('--n-repetitions', type=int, default=4)
 
     args = parser.parse_args()
 
     # dataset = DynamicsDataset(args.dataset_dir)
     dataset = ClassifierDataset(args.dataset_dir)
 
-    batch_size = 64
-
     t0 = perf_counter()
     tf_dataset = dataset.get_datasets(mode=args.mode)
+    batch_size = 128
     tf_dataset = tf_dataset.batch(batch_size)
 
-    # tf_dataset = add_transition_image(tf_dataset,
-    #                                   states_keys=["link_bot"],
-    #                                   scenario=get_scenario("link_bot"),
-    #                                   local_env_h=50,
-    #                                   local_env_w=50,
-    #                                   rope_image_k=1000)
     tf_dataset = tf_dataset.shuffle(512)
 
     time_to_load = perf_counter() - t0
     print("Time to Load (s): {:5.3f}".format(time_to_load))
 
-    # tf_dataset = tf_dataset.cache(cachename())
-    n_positive = 0
     try:
+        ram_usage = []
         for _ in range(args.n_repetitions):
             t0 = perf_counter()
-            for e in progressbar.progressbar(tf_dataset):
+            for e in progressbar.progressbar(tf_dataset.take(800)):
                 # print('{:.5f}'.format(perf_counter() - t0))
+                process = psutil.Process(os.getpid())
+                current_ram_usage = process.memory_info().vms
+                ram_usage.append(current_ram_usage)
                 pass
             print('{:.5f}'.format(perf_counter() - t0))
+        plt.plot(ram_usage)
+        plt.xlabel("iteration")
+        plt.ylabel("ram usage (bytes)")
+        plt.show()
     except KeyboardInterrupt:
         pass
 
