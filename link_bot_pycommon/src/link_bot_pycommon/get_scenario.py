0a5ae7b320030777e89d0066b051e0bacf6838a2
Peter Mitrano <pmitrano@umich.edu>
2020-06-27 15:09:44 -0400

    armada

--- link_bot_pycommon/src/link_bot_pycommon/get_scenario.py
+++ link_bot_pycommon/src/link_bot_pycommon/get_scenario.py
@@ -2,14 +2,13 @@ from typing import Dict
 
 from link_bot_pycommon.dual_floating_gripper_scenario import DualFloatingGripperRopeScenario
 from link_bot_pycommon.experiment_scenario import ExperimentScenario
-from link_bot_pycommon.link_bot_scenario import LinkBotScenario
 
 
 def get_scenario(params: Dict) -> ExperimentScenario:
     scenario_name = params['scenario']
     data_collection_params = params['data_collection_params']
     if scenario_name == 'link_bot':
-        return LinkBotScenario(data_collection_params)
+        raise NotImplementedError()
     elif scenario_name == 'dual_floating_gripper_rope':
         return DualFloatingGripperRopeScenario(data_collection_params)
     elif scenario_name == 'dual':
