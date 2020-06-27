10df0288c97e38ccb379ab76d1d1665a0ce57c0b
Peter Mitrano <pmitrano@umich.edu>
2020-06-27 00:49:02 -0400

    expose interface for using command gui

--- link_bot_gazebo/plugins/kinematic_victor_plugin.cpp
+++ link_bot_gazebo/plugins/kinematic_victor_plugin.cpp
@@ -1,6 +1,6 @@
 #include "kinematic_victor_plugin.h"
 
-#include <sensor_msgs/JointState.h>
+#include <ros/subscribe_options.h>
 #include <std_msgs/Empty.h>
 
 #include <algorithm>
@@ -14,12 +14,16 @@
 #define create_service_options_private(type, name, bind)                                                               \
   ros::AdvertiseServiceOptions::create<type>(name, bind, ros::VoidPtr(), &private_queue_)
 
-#define CANARY do { std::cerr << "Reached " << __FILE__ << ":" << __LINE__ << std::endl; } while(false);
+#define CANARY                                                                                                         \
+  do                                                                                                                   \
+  {                                                                                                                    \
+    std::cerr << "Reached " << __FILE__ << ":" << __LINE__ << std::endl;                                               \
+  } while (false);
 
-static std::istream& operator>>(std::istream &_in, std::vector<double>&_vec)
+static std::istream &operator>>(std::istream &_in, std::vector<double> &_vec)
 {
   // Remove any commas from the data
-  std::string content{std::istreambuf_iterator<char>(_in), std::istreambuf_iterator<char>()};
+  std::string content{ std::istreambuf_iterator<char>(_in), std::istreambuf_iterator<char>() };
   content.erase(std::remove(content.begin(), content.end(), ','), content.end());
 
   // FIXME: error handling
@@ -35,7 +39,8 @@ static std::istream& operator>>(std::istream &_in, std::vector<double>&_vec)
   return _in;
 }
 
-namespace gazebo {
+namespace gazebo
+{
 GZ_REGISTER_MODEL_PLUGIN(KinematicVictorPlugin)
 
 KinematicVictorPlugin::~KinematicVictorPlugin()
@@ -50,7 +55,8 @@ KinematicVictorPlugin::~KinematicVictorPlugin()
 
 void KinematicVictorPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf)
 {
-  if (!ros::isInitialized()) {
+  if (!ros::isInitialized())
+  {
     int argc = 0;
     ros::init(argc, nullptr, model_->GetScopedName(), ros::init_options::NoSigintHandler);
   }
@@ -92,8 +98,9 @@ void KinematicVictorPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf)
         else
         {
           auto const arm_prefix = "victor::victor_" + arm->Get<std::string>("name") + "_arm_joint_";
-          for (auto const &[idx, val] : enumerate(vals)) {
-            auto const joint_name = arm_prefix + std::to_string(idx+1);
+          for (auto const &[idx, val] : enumerate(vals))
+          {
+            auto const joint_name = arm_prefix + std::to_string(idx + 1);
             model_->GetJoint(joint_name)->SetPosition(0, val);
           }
         }
@@ -138,19 +145,40 @@ void KinematicVictorPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf)
   {
     private_ros_node_ = std::make_unique<ros::NodeHandle>(model_->GetScopedName());
     joint_states_pub_ = ros_node_.advertise<sensor_msgs::JointState>("joint_states", 10);
+    left_arm_motion_status_pub_ =
+        ros_node_.advertise<victor_hardware_interface::MotionStatus>("left_arm/motion_status", 10);
+    right_arm_motion_status_pub_ =
+        ros_node_.advertise<victor_hardware_interface::MotionStatus>("right_arm/motion_status", 10);
+    left_gripper_status_pub_ =
+        ros_node_.advertise<victor_hardware_interface::Robotiq3FingerStatus>("left_arm/gripper_status", 10);
+    right_gripper_status_pub_ =
+        ros_node_.advertise<victor_hardware_interface::Robotiq3FingerStatus>("right_arm/gripper_status", 10);
+    auto left_arm_motion_command_sub_options = ros::SubscribeOptions::create<victor_hardware_interface::MotionCommand>(
+        "left_arm/motion_command", 10, boost::bind(&KinematicVictorPlugin::OnLeftArmMotionCommand, this, _1),
+        ros::VoidPtr(), &queue_);
+    left_arm_motion_command_sub_ = ros_node_.subscribe(left_arm_motion_command_sub_options);
+    auto right_arm_motion_command_sub_options = ros::SubscribeOptions::create<victor_hardware_interface::MotionCommand>(
+        "right_arm/motion_command", 10, boost::bind(&KinematicVictorPlugin::OnRightArmMotionCommand, this, _1),
+        ros::VoidPtr(), &queue_);
+    right_arm_motion_command_sub_ = ros_node_.subscribe(right_arm_motion_command_sub_options);
     auto execute = [this](const TrajServer::GoalConstPtr &goal) { this->FollowJointTrajectory(goal); };
     follow_traj_server_ = std::make_unique<TrajServer>(ros_node_, "follow_joint_trajectory", execute, false);
     follow_traj_server_->start();
 
     ros_queue_thread_ = std::thread([this] { QueueThread(); });
     private_ros_queue_thread_ = std::thread([this] { PrivateQueueThread(); });
-
-    auto update = [this](common::UpdateInfo const &/*info*/) { OnUpdate(); };
-    update_connection_ = event::Events::ConnectWorldUpdateBegin(update);
   }
+
+  periodic_event_thread_ = std::thread([this] {
+    while (true)
+    {
+      PeriodicUpdate();
+      sleep(1);
+    }
+  });
 }
 
-void KinematicVictorPlugin::OnUpdate()
+void KinematicVictorPlugin::PeriodicUpdate()
 {
   // Remove the leading "victor::" from the joint names
   auto const n_removed = std::strlen("victor::");
@@ -158,7 +186,8 @@ void KinematicVictorPlugin::OnUpdate()
   for (auto const &j : model_->GetJoints())
   {
     // FIXME: why is this not equal to physics::Joint::HINGE_JOINT??
-    if (j->GetType() == 576) {  // revolute
+    if (j->GetType() == 576)
+    {  // revolute
       msg.name.push_back(j->GetName().substr(n_removed));
       msg.position.push_back(j->Position(0));
       msg.velocity.push_back(j->GetVelocity(0));
@@ -167,6 +196,127 @@ void KinematicVictorPlugin::OnUpdate()
   }
   msg.header.stamp = ros::Time::now();
   joint_states_pub_.publish(msg);
+
+  PublishLeftArmMotionStatus();
+  PublishRightArmMotionStatus();
+
+  PublishLeftGripperStatus();
+  PublishRightGripperStatus();
+}
+
+void KinematicVictorPlugin::PublishLeftGripperStatus()
+{
+  victor_hardware_interface::Robotiq3FingerStatus status;
+  left_gripper_status_pub_.publish(status);
+}
+
+void KinematicVictorPlugin::PublishRightGripperStatus()
+{
+  victor_hardware_interface::Robotiq3FingerStatus status;
+  right_gripper_status_pub_.publish(status);
+}
+
+void KinematicVictorPlugin::PublishLeftArmMotionStatus()
+{
+  victor_hardware_interface::MotionStatus left_arm_motion_status;
+  std::vector<double *> joint_angles{
+    &left_arm_motion_status.measured_joint_position.joint_1, &left_arm_motion_status.measured_joint_position.joint_2,
+    &left_arm_motion_status.measured_joint_position.joint_3, &left_arm_motion_status.measured_joint_position.joint_4,
+    &left_arm_motion_status.measured_joint_position.joint_5, &left_arm_motion_status.measured_joint_position.joint_6,
+    &left_arm_motion_status.measured_joint_position.joint_7,
+  };
+  left_arm_motion_status_pub_.publish(left_arm_motion_status);
+  for (auto joint_idx{ 1u }; joint_idx <= 7u; ++joint_idx)
+  {
+    std::stringstream joint_name_ss;
+    joint_name_ss << "victor::victor_left_arm_joint_" << joint_idx;
+    auto joint = model_->GetJoint(joint_name_ss.str());
+    if (joint)
+    {
+      *joint_angles[joint_idx - 1] = joint->Position(0);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Failed to get joint with name " << joint_name_ss.str());
+      return;
+    }
+  }
+  left_arm_motion_status.header.stamp = ros::Time::now();
+  left_arm_motion_status_pub_.publish(left_arm_motion_status);
+}
+
+void KinematicVictorPlugin::PublishRightArmMotionStatus()
+{
+  victor_hardware_interface::MotionStatus right_arm_motion_status;
+  std::vector<double *> joint_angles{
+    &right_arm_motion_status.measured_joint_position.joint_1, &right_arm_motion_status.measured_joint_position.joint_2,
+    &right_arm_motion_status.measured_joint_position.joint_3, &right_arm_motion_status.measured_joint_position.joint_4,
+    &right_arm_motion_status.measured_joint_position.joint_5, &right_arm_motion_status.measured_joint_position.joint_6,
+    &right_arm_motion_status.measured_joint_position.joint_7,
+  };
+  for (auto joint_idx{ 1u }; joint_idx <= 7u; ++joint_idx)
+  {
+    std::stringstream joint_name_ss;
+    joint_name_ss << "victor::victor_right_arm_joint_" << joint_idx;
+    auto joint = model_->GetJoint(joint_name_ss.str());
+    if (joint)
+    {
+      *joint_angles[joint_idx - 1] = joint->Position(0);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Failed to get joint with name " << joint_name_ss.str());
+      return;
+    }
+  }
+  right_arm_motion_status.header.stamp = ros::Time::now();
+  right_arm_motion_status_pub_.publish(right_arm_motion_status);
+}
+
+void KinematicVictorPlugin::OnLeftArmMotionCommand(const victor_hardware_interface::MotionCommandConstPtr &msg)
+{
+  std::vector<double> joint_angles{
+    msg->joint_position.joint_1, msg->joint_position.joint_2, msg->joint_position.joint_3, msg->joint_position.joint_4,
+    msg->joint_position.joint_5, msg->joint_position.joint_6, msg->joint_position.joint_7,
+  };
+  for (auto joint_idx{ 1u }; joint_idx <= 7u; ++joint_idx)
+  {
+    std::stringstream joint_name_ss;
+    joint_name_ss << "victor::victor_left_arm_joint_" << joint_idx;
+    auto joint = model_->GetJoint(joint_name_ss.str());
+    if (joint)
+    {
+      joint->SetPosition(0, joint_angles[joint_idx - 1]);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Failed to get joint with name " << joint_name_ss.str());
+      return;
+    }
+  }
+}
+
+void KinematicVictorPlugin::OnRightArmMotionCommand(const victor_hardware_interface::MotionCommandConstPtr &msg)
+{
+  std::vector<double> joint_angles{
+    msg->joint_position.joint_1, msg->joint_position.joint_2, msg->joint_position.joint_3, msg->joint_position.joint_4,
+    msg->joint_position.joint_5, msg->joint_position.joint_6, msg->joint_position.joint_7,
+  };
+  for (auto joint_idx{ 1u }; joint_idx <= 7u; ++joint_idx)
+  {
+    std::stringstream joint_name_ss;
+    joint_name_ss << "victor::victor_right_arm_joint_" << joint_idx;
+    auto joint = model_->GetJoint(joint_name_ss.str());
+    if (joint)
+    {
+      joint->SetPosition(0, joint_angles[joint_idx - 1]);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Failed to get joint with name " << joint_name_ss.str());
+      return;
+    }
+  }
 }
 
 void KinematicVictorPlugin::FollowJointTrajectory(const TrajServer::GoalConstPtr &goal)
@@ -178,21 +328,24 @@ void KinematicVictorPlugin::FollowJointTrajectory(const TrajServer::GoalConstPtr
   auto const seconds_per_step = model_->GetWorld()->Physics()->GetMaxStepSize();
   auto const settling_time_seconds = goal->goal_time_tolerance.toSec();
   auto const steps = static_cast<unsigned int>(settling_time_seconds / seconds_per_step);
-  ROS_INFO_STREAM("Received trajectory with " <<
-                  "seconds_per_step: " << seconds_per_step <<
-                  "  settling_time_seconds: " << settling_time_seconds <<
-                  "  steps per point: " << steps <<
-                  "  points: " << goal->trajectory.points.size());
-  for (auto const &point : goal->trajectory.points) {
+  ROS_INFO_STREAM("Received trajectory with "
+                  << "seconds_per_step: " << seconds_per_step << "  settling_time_seconds: " << settling_time_seconds
+                  << "  steps per point: " << steps << "  points: " << goal->trajectory.points.size());
+  for (auto const &point : goal->trajectory.points)
+  {
     // Move Victor to the specified joint configuration
-    for (auto const &pair : enumerate(goal->trajectory.joint_names)) {
+    for (auto const &pair : enumerate(goal->trajectory.joint_names))
+    {
       auto const &[joint_idx, joint_name] = pair;
       auto joint = model_->GetJoint("victor::" + joint_name);
-      if (joint) {
+      if (joint)
+      {
         joint->SetPosition(0, point.positions[joint_idx]);
       }
-      else {
-        gzerr << "Invalid joint: " << "victor::" + joint_name << std::endl;
+      else
+      {
+        gzerr << "Invalid joint: "
+              << "victor::" + joint_name << std::endl;
         result.error_code = control_msgs::FollowJointTrajectoryResult::INVALID_JOINTS;
         follow_traj_server_->setSucceeded(result);
         world_->SetPaused(false);
--- link_bot_gazebo/plugins/kinematic_victor_plugin.h
+++ link_bot_gazebo/plugins/kinematic_victor_plugin.h
@@ -4,6 +4,10 @@
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include <ros/callback_queue.h>
 #include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <victor_hardware_interface/MotionCommand.h>
+#include <victor_hardware_interface/MotionStatus.h>
+#include <victor_hardware_interface/Robotiq3FingerStatus.h>
 
 #include <gazebo/common/Events.hh>
 #include <gazebo/common/Plugin.hh>
@@ -11,24 +15,35 @@
 #include <gazebo/physics/physics.hh>
 #include <thread>
 
-namespace gazebo {
-
+namespace gazebo
+{
 using TrajServer = actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction>;
 
-class KinematicVictorPlugin : public ModelPlugin {
- public:
+class KinematicVictorPlugin : public ModelPlugin
+{
+public:
   ~KinematicVictorPlugin() override;
 
   void Load(physics::ModelPtr parent, sdf::ElementPtr sdf) override;
 
   void FollowJointTrajectory(const TrajServer::GoalConstPtr &goal);
 
- private:
+  void OnLeftArmMotionCommand(const victor_hardware_interface::MotionCommandConstPtr &msg);
+
+  void OnRightArmMotionCommand(const victor_hardware_interface::MotionCommandConstPtr &msg);
+
+  void PublishLeftArmMotionStatus();
+  void PublishRightArmMotionStatus();
+
+  void PublishLeftGripperStatus();
+  void PublishRightGripperStatus();
+
+private:
   void QueueThread();
 
   void PrivateQueueThread();
 
-  void OnUpdate();
+  void PeriodicUpdate();
 
   event::ConnectionPtr update_connection_;
   physics::ModelPtr model_;
@@ -40,13 +55,20 @@ class KinematicVictorPlugin : public ModelPlugin {
   ros::CallbackQueue private_queue_;
   std::thread ros_queue_thread_;
   std::thread private_ros_queue_thread_;
+  std::thread periodic_event_thread_;
   ros::Publisher joint_states_pub_;
+  ros::Publisher left_arm_motion_status_pub_;
+  ros::Publisher right_arm_motion_status_pub_;
+  ros::Publisher left_gripper_status_pub_;
+  ros::Publisher right_gripper_status_pub_;
+  ros::Subscriber left_arm_motion_command_sub_;
+  ros::Subscriber right_arm_motion_command_sub_;
 
   std::unique_ptr<TrajServer> follow_traj_server_;
-  std::string left_flange_name_{"victor::victor_left_arm_link_7"};
-  std::string right_flange_name_{"victor::victor_right_arm_link_7"};
-  std::string gripper1_name_{"link_bot::gripper1"};
-  std::string gripper2_name_{"link_bot::gripper2"};
+  std::string left_flange_name_{ "victor::victor_left_arm_link_7" };
+  std::string right_flange_name_{ "victor::victor_right_arm_link_7" };
+  std::string gripper1_name_{ "link_bot::gripper1" };
+  std::string gripper2_name_{ "link_bot::gripper2" };
   physics::LinkPtr left_flange_;
   physics::LinkPtr right_flange_;
   physics::LinkPtr gripper1_;
