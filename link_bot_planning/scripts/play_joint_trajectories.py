df72473fca4a7efb8f1d870744f72d4fbbf468d1
Peter Mitrano <pmitrano@umich.edu>
2020-07-27 16:12:43 -0400

    record some gazebo playback

--- link_bot_planning/scripts/play_joint_trajectories.py
+++ link_bot_planning/scripts/play_joint_trajectories.py
@@ -10,11 +10,13 @@ import rosbag
 from control_msgs.msg import FollowJointTrajectoryAction
 from arm_video_recorder.srv import TriggerVideoRecording, TriggerVideoRecordingRequest
 
+
 def main():
     rospy.init_node('follow_joint_trajectory_client')
 
     parser = argparse.ArgumentParser()
     parser.add_argument("bagfile")
+    parser.add_argument("--no-video", action='store_true')
 
     args = parser.parse_args()
 
@@ -23,10 +25,11 @@ def main():
     start_msg = TriggerVideoRecordingRequest()
     parent_dir = os.path.join(*os.path.split(args.bagfile)[:-1])
     filename = os.path.join(parent_dir, "trajectory_playback-" + str(int(time.time())) + '.avi')
-    start_msg.record = True
-    start_msg.filename = filename
-    start_msg.timeout_in_sec = 600.0
-    record(start_msg)
+    if not args.no_video:
+        start_msg.record = True
+        start_msg.filename = filename
+        start_msg.timeout_in_sec = 600.0
+        record(start_msg)
 
     client = actionlib.SimpleActionClient('both_arms_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
     client.wait_for_server()
@@ -46,9 +49,11 @@ def main():
 
     bag.close()
 
-    stop_msg = TriggerVideoRecordingRequest()
-    stop_msg.record = False
-    record(stop_msg)
+    if not args.no_video:
+        stop_msg = TriggerVideoRecordingRequest()
+        stop_msg.record = False
+        record(stop_msg)
+
 
 if __name__ == '__main__':
     main()
