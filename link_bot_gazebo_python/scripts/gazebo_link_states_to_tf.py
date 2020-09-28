a7666074cc8645bc97296a36606abac699d6def6
Peter <pmitrano@storm20.04>
2020-09-28 10:56:00 -0400

    script to forward gazebo link state poses to TF

---
+++ link_bot_gazebo_python/scripts/gazebo_link_states_to_tf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import pathlib
+
+import colorama
+import hjson
+
+import ros_numpy
+import rospy
+import tf
+from gazebo_msgs.msg import LinkStates
+
+
+class GazeboLinkStatesToTF:
+
+    def __init__(self):
+        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.link_states_sub = rospy.Subscriber('gazebo/link_states', LinkStates, self.on_link_states)
+        config_file = pathlib.Path(rospy.get_param("~config_file", None))
+        if config_file is None:
+            self.excluded_names = []
+        else:
+            with config_file.open('r') as config_file:
+                config = hjson.load(config_file)
+            self.excluded_names = config['exclude']
+
+    def on_link_states(self, msg: LinkStates):
+        for link_name, link_pose in zip(msg.name, msg.pose):
+            translation = ros_numpy.numpify(link_pose.position)
+            rotation = ros_numpy.numpify(link_pose.orientation)
+            child = "world"
+            parent = link_name.replace("::", "/")
+            if link_name not in self.excluded_names:
+                self.tf_broadcaster.sendTransform(translation, rotation, rospy.Time.now(), parent, child)
+        rospy.sleep(0.1)
+
+
+def main():
+    colorama.init(autoreset=True)
+    rospy.init_node("gazebo_link_states_to_tf")
+
+    GazeboLinkStatesToTF()
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
