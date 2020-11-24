02498a01b759afa91c16455f76b29f52f908e1f5
Peter Mitrano <pmitrano@armstorm>
2020-11-24 16:52:46 -0500

    more trials
    make imports only happen when they're used

--- link_bot_pycommon/src/link_bot_pycommon/get_service_provider.py
+++ link_bot_pycommon/src/link_bot_pycommon/get_service_provider.py
@@ -1,11 +1,9 @@
-from link_bot_gazebo_python.gazebo_services import GazeboServices
-from victor.victor_services import VictorServices
-
-
 def get_service_provider(service_provider_name):
     if service_provider_name == 'victor':
+        from victor.victor_services import VictorServices
         return VictorServices()
     elif service_provider_name == 'gazebo':
+        from link_bot_gazebo_python.gazebo_services import GazeboServices
         return GazeboServices()
     else:
         raise NotImplementedError()
